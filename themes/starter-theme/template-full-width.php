bd7705d1d55528aa3f4a6382a337ef0ffd376a5f
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-17 22:54:52 -0400

    full width template

---
+++ themes/starter-theme/template-full-width.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * This is a full width page template (no sidebar).
+ *
+ * @package Starter_Theme
+ */
+
+get_header(); ?>
+
+<section id="primary" class="full-width" role="main">
+
+   <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header><!-- .entry-header -->
+
+            <div class="entry-content">
+                <?php the_content(); ?>
+                <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
+            </div><!-- .entry-content -->
+        </article><!-- #post-<?php the_ID(); ?> -->
+
+    <?php endwhile; // end of the loop. ?>
+
+</section><!-- #primary -->
+
+<?php get_footer(); ?>
