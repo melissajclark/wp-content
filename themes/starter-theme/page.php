db6043420c93b0fb7461939e58b57dd96b535c31
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-17 22:50:52 -0400

    initalized page template

---
+++ themes/starter-theme/page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The Page template
+ *
+ * This is the template that displays all pages by default.
+ *
+ * @package Starter_Theme
+ */
+
+get_header(); ?>
+
+<section id="primary" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header><!-- .entry-header -->
+
+            <div class="entry-content">
+                <?php the_content(); ?>
+                <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
+            </div><!-- .entry-content -->
+        </article><!-- #post-<?php the_ID(); ?> -->
+
+    <?php endwhile; // end of the loop. ?>
+
+</section><!-- #primary -->
+
+<?php get_footer(); ?>
