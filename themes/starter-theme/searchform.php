ede352fa699ce788e15676c96efa12dda50cc2e7
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-17 21:55:24 -0400

    initiated searchform file

---
+++ themes/starter-theme/searchform.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Search form template
+ *
+ * @package Starter_Theme
+ */
+?>
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <label for="s" class="assistive-text"><?php _e( 'Search', 'starter-theme' ); ?></label>
+    <input type="text" class="field" name="s" id="s" placeholder="" />
+    <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'SEARCH', 'starter-theme' ); ?>" />
+</form>
