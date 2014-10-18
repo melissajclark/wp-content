2c361a484ec4b9d5d68465029b89822fc3c9e57d
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-17 22:08:02 -0400

    updated search output with commenting on whats what

--- themes/starter-theme/search.php
+++ themes/starter-theme/search.php
@@ -11,11 +11,27 @@ get_header(); ?>
 
     <?php if ( have_posts() ) : ?>
 
-        <header class="archive-header">
-            <h1 class="archive-title">
-                <?php printf( __( 'Search Results for: %s', 'starter-theme' ), '<span>' . get_search_query() . '</span>' ); ?>
-            </h1>
-        </header>
+    <header class="archive-header">
+    
+        <!-- option 1 -->
+        <!-- normal format (search results for: .....) -->
+
+        <!-- <h1 class="archive-title">
+            <?php printf( __( 'Search Results for: %s', 'starter-theme' ), '<span>' . get_search_query() . '</span>' ); ?>
+        </h1> -->
+        
+        <!-- option 2 -->
+        <!-- fancier format: displays search term in quotes + number of results -->
+
+        <?php
+        global $wp_query;
+        $total_results = $wp_query->found_posts;
+        ?>
+
+        <h1>Search Results</h1>
+        <h2><?php echo $total_results; ?> results found for &#8220;<?php echo get_search_query(); ?>&#8221;</h2>
+
+    </header>
 
         <?php /* Start the Loop */ ?>
         <?php while ( have_posts() ) : the_post(); ?>
