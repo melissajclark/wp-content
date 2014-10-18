4ab15a59e341558839416276e7ce5d2e951a8857
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-17 21:59:28 -0400

    initated search file

---
+++ themes/starter-theme/search.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying Search Results pages.
+ *
+ * @package Starter_Theme
+ */
+
+get_header(); ?>
+
+<section id="primary" role="main">
+
+    <?php if ( have_posts() ) : ?>
+
+        <header class="archive-header">
+            <h1 class="archive-title">
+                <?php printf( __( 'Search Results for: %s', 'starter-theme' ), '<span>' . get_search_query() . '</span>' ); ?>
+            </h1>
+        </header>
+
+        <?php /* Start the Loop */ ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+
+            <?php
+                get_template_part( 'content');
+            ?>
+
+        <?php endwhile; ?>
+
+        <?php get_template_part( 'inc/pagination' ); ?>
+
+    <?php else : ?>
+
+        <?php get_template_part( 'content', 'none' ); ?>
+
+    <?php endif; ?>
+
+</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
