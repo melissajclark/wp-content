222bece3daf792fc4d60f3afe18b010f9cadd42a
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-17 21:38:36 -0400

    added sidebar for blog

--- themes/starter-theme/functions.php
+++ themes/starter-theme/functions.php
@@ -79,7 +79,16 @@ function starter_theme_widgets_init() {
     ) );
     register_sidebar( array(
         'name' => __( 'Footer Widget Area', 'starter-theme' ),
-        'id' => 'footer-area',
+        'id' => 'footer-area', 
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<span style="display:none;">',
+        'after_title' => '</span>',
+    ) );
+
+    register_sidebar( array(
+        'name' => __( 'Blog Page Sidebar', 'starter-theme' ),
+        'id' => 'sidebar-blog', 
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget' => '</aside>',
         'before_title' => '<span style="display:none;">',
