f003dcbabab30fd32e19d5a0fbf44b6f56338f8c
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-17 21:35:53 -0400

    initiated sidebar

---
+++ themes/starter-theme/sidebar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The main sidebar
+ *
+ * @package Starter_Theme
+ */
+
+?>
+<div id="secondary" class="widget-area" role="complementary">
+    <?php dynamic_sidebar( 'sidebar-1' ); ?>
+</div><!-- #secondary .widget-area -->
