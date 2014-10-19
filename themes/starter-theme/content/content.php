2e770829f21731a1b618e963f0b9a1aa2c2a756e
“Melissa” <melissa@melissajclark.ca>
2014-10-19 14:51:20 -0400

    created folder for content includes

---
+++ themes/starter-theme/content/content.php
@@ -0,0 +1,26 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <h1 class="entry-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h1>
+        <span class="entry-date"><?php echo get_the_date(); ?></span>
+    </header><!-- .entry-header -->
+
+    <div class="entry-content">
+        <?php the_content(); ?>
+        <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'starter-theme' ) . '</span>', 'after' => '</div>' ) ); ?>
+    </div><!-- .entry-content -->
+
+    <footer class="entry-meta">
+
+        <?php the_tags( '<div class="post-tags">' . __( 'Tagged: ', 'starter-theme' ) , ', ', '</div>' ); ?>
+
+        <div class="comments-link">
+            <?php comments_popup_link( 
+                 __( 'Leave a comment', 'starter-theme' ), 
+                 __( '1 comment', 'starter-theme' ), 
+                 __( '% comments', 'starter-theme' ) ); 
+            ?>
+        </div>
+    </footer><!-- #entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
