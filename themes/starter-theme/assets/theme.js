59376e1457959c6f5129a58c06030a566b4b860d
“Melissa” <melissa@melissajclark.ca>
2014-10-19 16:42:29 -0400

    updated slider options

--- themes/starter-theme/assets/theme.js
+++ themes/starter-theme/assets/theme.js
@@ -1,12 +1,8 @@
 function slider(){
     jQuery(document).ready(function($){
         $('.bxslider').bxSlider({
-            mode: 'fade',
-            pager: true,
-            nextSelector: '#slider-next',
-            prevSelector: '#slider-prev',
-            nextText: 'Onward →',
-            prevText: '← Go back'
+          auto: true,
+          autoControls: true
         });
     });
 }
