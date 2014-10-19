cbd93f26887ecb7a06e49570f404729f242a38fd
“Melissa” <melissa@melissajclark.ca>
2014-10-19 17:22:48 -0400

    updated with wooslider js

--- themes/starter-theme/assets/theme.js
+++ themes/starter-theme/assets/theme.js
@@ -1,3 +1,5 @@
+
+// bXslider function
 function sliderBX(){
     jQuery(document).ready(function($){
         $('.bxslider').bxSlider({
@@ -8,3 +10,27 @@ function sliderBX(){
         });
     });
 }
+
+function wooSlider(){
+jQuery(document).ready(function($){
+$(window).load(function() {
+  // The slider being synced must be initialized first
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider'
+  });
+   
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel"
+  });
+});
+} // closes required WordPress wrapper
