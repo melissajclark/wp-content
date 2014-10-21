1be0ecc9b57935a00090d51342dedd4b99798762
“Melissa” <melissa@melissajclark.ca>
2014-10-21 15:49:03 -0400

    still trying to get the thumnbanils working

--- themes/starter-theme/assets/theme.js
+++ themes/starter-theme/assets/theme.js
@@ -1,26 +1,22 @@
 
 // bXslider function
-function sliderBX(){
-    jQuery(document).ready(function($){
-        $('.bxslider').bxSlider({
-          infiniteLoop: false,
-            hideControlOnEnd: true,
-            adaptiveHeight: true,
-            responsive: true
-        });
-    });
-}
+// function sliderBX(){
+//     jQuery(document).ready(function($){
+//         $('.bxslider').bxSlider({
+//           infiniteLoop: false,
+//             hideControlOnEnd: true,
+//             adaptiveHeight: true,
+//             responsive: true
+//         });
+//     });
+// }
 
 function wooSlider(){
   jQuery(document).ready(function($) {
     $(document).ready(function() {
      $('.flexslider').flexslider({
        animation: "slide",
-       animationLoop: false,
-       // itemHeight: 550,
-       // itemMargin: 5,
-       pausePlay: true,
-       controlNav: true
+       controlNav: "thumbnails"
      });
     });
   }); // jQuery ready function
