b39bab021d86910a5c55e3356719845b3be227be
“Melissa” <melissa@melissajclark.ca>
2014-10-21 16:07:51 -0400

    copied exact code from http://flexslider.woothemes.com/thumbnail-slider.html and it works

--- themes/starter-theme/assets/theme.js
+++ themes/starter-theme/assets/theme.js
@@ -2,7 +2,26 @@ function wooSlider() {
 
   jQuery(document).ready(function($){
 
-
+    $(window).load(function() {
+      // initalize slider first
+      $('#carousel').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        asNavFor: '#slider'
+      });
+     
+      $('#slider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#carousel"
+      });
+    });
 
   }); // end of WordPress doc ready function
 
