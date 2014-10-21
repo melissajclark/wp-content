986529e5bbddef6ffbfc31e2c720a8d734ceae7e
“Melissa” <melissa@melissajclark.ca>
2014-10-21 16:05:17 -0400

    starting fresh

--- themes/starter-theme/assets/theme.js
+++ themes/starter-theme/assets/theme.js
@@ -11,28 +11,12 @@
 //     });
 // }
 
-function wooSlider(){
-  jQuery(document).ready(function($){
-    $(document).load(function() {
-      // The slider being synced must be initialized first
-      $("#carousel").flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        itemWidth: 210,
-        itemMargin: 5,
-        asNavFor: "#slider"
-      });
-       
-      $("#slider").flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        sync: "#carousel"
-      });
-    // });
+function wooSlider() {
 
-  }); // jQuery ready function
-}); // end wooSlider function
+  jQuery(document).ready(function($){
+
+
+
+  }); // end of WordPress doc ready function
+
+} // end of wooslider function
