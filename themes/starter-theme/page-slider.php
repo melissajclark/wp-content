1c1ab24d3554f5d37182e1ae00440eda34806cf9
“Melissa” <melissa@melissajclark.ca>
2014-10-21 15:55:12 -0400

    still not working

--- themes/starter-theme/page-slider.php
+++ themes/starter-theme/page-slider.php
@@ -33,7 +33,7 @@ get_header(); ?>
                             </li>
                         <?php endforeach; ?>
                     </ul>
-                
+                </div>
                 <div id="carousel" class="flexslider">
                     <ul class="slides">
                         <?php foreach( $images as $image ): ?>
@@ -43,7 +43,7 @@ get_header(); ?>
                         <?php endforeach; ?>
                     </ul>
                 </div>
-            </div>
+            
             <?php endif; ?>
 
 
