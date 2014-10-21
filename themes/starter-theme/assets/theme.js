2920ece5f5b4e8505ffa427d3c4046ec53418702
“Melissa” <melissa@melissajclark.ca>
2014-10-21 15:54:52 -0400

    using code from example http://flexslider.woothemes.com/thumbnail-slider.html

--- themes/starter-theme/assets/theme.js
+++ themes/starter-theme/assets/theme.js
@@ -13,11 +13,26 @@
 
 function wooSlider(){
   jQuery(document).ready(function($) {
-    $(document).ready(function() {
-     $('.flexslider').flexslider({
-       animation: "slide",
-       controlNav: "thumbnails"
-     });
+    $(document).load(function() {
+      // The slider being synced must be initialized first
+      $('#carousel').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        asNavFor: '#slider'
+      });
+       
+      $('#slider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#carousel"
+      });
     });
+
   }); // jQuery ready function
-} // end wooSlider function
+}); // end wooSlider function
