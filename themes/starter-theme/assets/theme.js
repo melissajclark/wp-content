32ba02a3bcd216d0190ff171d6751c90f9d0b0db
“Melissa” <melissa@melissajclark.ca>
2014-10-21 16:05:32 -0400

    deleted bxslider stuff

--- themes/starter-theme/assets/theme.js
+++ themes/starter-theme/assets/theme.js
@@ -1,16 +1,3 @@
-
-// bXslider function
-// function sliderBX(){
-//     jQuery(document).ready(function($){
-//         $('.bxslider').bxSlider({
-//           infiniteLoop: false,
-//             hideControlOnEnd: true,
-//             adaptiveHeight: true,
-//             responsive: true
-//         });
-//     });
-// }
-
 function wooSlider() {
 
   jQuery(document).ready(function($){
