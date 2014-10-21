70dbdb0dc72814dec77e314e5842bbcae86dfe66
“Melissa” <melissa@melissajclark.ca>
2014-10-21 16:34:13 -0400

    smoothHeight:true

--- themes/starter-theme/assets/theme.js
+++ themes/starter-theme/assets/theme.js
@@ -32,24 +32,14 @@ function wooSlider() {
 
   jQuery(document).ready(function($){
 
-    $(window).load(function() {
-      // initalize slider first
-      // $("#carousel").flexslider({
-      //   animation: "slide",
-      //   controlNav: false,
-      //   animationLoop: false,
-      //   slideshow: false,
-      //   itemWidth: 100,
-      //   itemMargin: 10,
-      //   asNavFor: "#slider"
-      // });
-     
+    $(window).load(function() {   
       $("#slider").flexslider({
         animation: "fade",
         controlNav: false,
         animationLoop: false,
         slideshow: false,
-        // sync: "#carousel"
+        smoothHeight: true
+        
       });
     });
 
