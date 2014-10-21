8468887fffa91cc1c83cf4210a68c1d0169aec4f
“Melissa” <melissa@melissajclark.ca>
2014-10-21 16:32:31 -0400

    updated, left code for slider with carusel, new function for just slider

--- themes/starter-theme/assets/theme.js
+++ themes/starter-theme/assets/theme.js
@@ -1,3 +1,33 @@
+function wooSliderAndCarousel() {
+
+  jQuery(document).ready(function($){
+
+    $(window).load(function() {
+      // initalize slider first
+      $("#carousel").flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 100,
+        itemMargin: 10,
+        asNavFor: "#slider"
+      });
+     
+      $("#slider").flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#carousel"
+      });
+    });
+
+  }); // end of WordPress doc ready function
+
+} // end of wooslider function
+
+
 function wooSlider() {
 
   jQuery(document).ready(function($){
@@ -15,7 +45,7 @@ function wooSlider() {
       // });
      
       $("#slider").flexslider({
-        animation: "slide",
+        animation: "fade",
         controlNav: false,
         animationLoop: false,
         slideshow: false,
