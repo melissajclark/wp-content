f4939f4c304147d418c9701be87e68afc35d5ef2
“Melissa” <melissa@melissajclark.ca>
2014-10-25 15:48:22 -0400

    removed wallopSlider

--- themes/starter-theme/page-slider.php
+++ themes/starter-theme/page-slider.php
@@ -64,45 +64,6 @@ get_header(); ?>
 
         <hr/>
 
-        <h3>Wallop Slider</h3>
-
-        <!-- Custom Field Content for Images -->
-
-        <div class="photo-slider wallop-slider wallop-slider--slide">
-
-            <?php 
-
-            $image = get_field('first_image');
-
-            if( !empty($image) ): ?>
-            <ul class="wallop-slider__list">
-                <li class="wallop-slider__item--current">
-                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                </li>
-
-            <?php endif; ?>            
-
-            <?php 
-
-            $images = get_field('gallery');
-
-            if( $images ): ?>
-                    
-                        <?php foreach( $images as $image ): ?>
-                            <li class="wallop-slider__item--current">
-                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                                <p><?php echo $image['caption']; ?></p>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-            <?php endif; ?>
-            <button class="wallop-slider__btn wallop-slider__btn--previous btn btn--previous" disabled="disabled">Previous</button>
-      <button class="wallop-slider__btn wallop-slider__btn--next btn btn--next">Next</button>
-
-    </div> <!-- end of wallopSlider -->
-
-        <hr/>
-
 
                 <?php the_content(); ?>
                 <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
