c269609e785d59a7101ad736f41cfccd1e51212a
“Melissa” <melissa@melissajclark.ca>
2014-10-25 16:56:39 -0400

    changed all img references to font-awesome icons

--- themes/starter-theme/inc/social-media-fields.php
+++ themes/starter-theme/inc/social-media-fields.php
@@ -8,7 +8,7 @@
 			
 			<?php if ( get_field( 'twitter', 'options' ) ) : ?>
 			<a href="<?php the_field( 'twitter', 'options' ); ?>" target="_blank" class="social-link" alt="twitter-link" >
-			    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/twitter-64.png" alt="twitter" class="social">
+			    <i class="fa fa-twitter fa-2x" alt="twitter" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
@@ -16,7 +16,7 @@
 
 			<?php if ( get_field( 'instagram', 'options' ) ) : ?>
 			<a href="<?php the_field( 'instagram', 'options' ); ?>" target="_blank" class="social-link" alt="instagram-profile">
-			    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/instagram-64.png" alt="instagram" class="social">
+			    <i class="fa fa-instagram fa-2x" alt="instagram" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
@@ -24,7 +24,7 @@
 
 			<?php if ( get_field( 'tumblr', 'options' ) ) : ?>
 			<a href="<?php the_field( 'tumblr', 'options' ); ?>" target="_blank" class="social-link" alt="tumblr-profile">
-			    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/tumblr-64.png" alt="tumblr" class="social">
+			    <i class="fa fa-tumblr fa-2x" alt="tumblr" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
@@ -32,7 +32,7 @@
 
 			<?php if ( get_field( 'linkedin', 'options' ) ) : ?>
 			<a href="<?php the_field( 'linkedin', 'options' ); ?>" target="_blank" class="social-link" alt="linkedin-profile">
-			    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/linkedin-64.png" alt="linkedin" class="social">
+			    <i class="fa fa-linkedin fa-2x" alt="linkedin" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
@@ -40,7 +40,7 @@
 
 			<?php if ( get_field( 'goodreads', 'options' ) ) : ?>
 			<a href="<?php the_field( 'goodreads', 'options' ); ?>" target="_blank" class="social-link" alt="good-reads-profile">
-			    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/goodreads-64.png" alt="goodreads" class="social">
+			    <i class="fa fa-book fa-2x" alt="goodreads" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
@@ -48,16 +48,15 @@
 
 			<?php if ( get_field( 'google_plus', 'options' ) ) : ?>
 			<a href="<?php the_field( 'google_plus', 'options' ); ?>" target="_blank" class="social-link" alt="google-plus-profile">
-			    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/google-plus-64.png" alt="googleplus" class="social">
+			    <i class="fa fa-google-plus fa-2x" alt="googleplus" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
 			<!-- Email -->
 
-
 			<?php if ( get_field( 'email', 'options' ) ) : ?>
 			 <a href="mailto:<?php the_field( 'email', 'options' ); ?>" target="_blank" class="social-link" alt="email-link">
-			    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/email-64.png" alt="email" class="social">
+			    <i class="fa fa fa-envelope-o fa-2x" alt="email" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
