bdf857a3c2727a7e6c3ed626f0237de79fb3dfe9
“Melissa” <melissa@melissajclark.ca>
2014-10-25 16:37:08 -0400

    new template for galleries - uses Glide Slider only

---
+++ themes/starter-theme/page-gallery.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template Name: Gallery Template
+ *
+ * This is the template that displays all pages by default.
+ *
+ * @package Starter_Theme
+ */
+
+get_header(); ?>
+
+<section id="primary" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header><!-- .entry-header -->
+
+            <div class="entry-content">
+
+             <h3>Glide Slider</h3>
+
+            <?php        // Custom Field Content for Images
+
+            $images = get_field('gallery');
+
+            if( $images ): ?>
+                <div class="slider">
+                    <ul class="slider__wrapper">
+                        <?php foreach( $images as $image ): ?>
+                            <li class="slider__item"><img class="glideSlideImage" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>"/>
+                                <p><?php echo $image['caption']; ?></p>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
+
+                <?php the_content(); ?>
+                <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
+            </div><!-- .entry-content -->
+        </article><!-- #post-<?php the_ID(); ?> -->
+
+    <?php endwhile; // end of the loop. ?>
+
+</section><!-- #primary -->
+
+<?php get_footer(); ?>
