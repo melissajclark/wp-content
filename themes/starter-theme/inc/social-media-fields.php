232d0a5cadd16e0f997050ec8de18f9c37fc103b
“Melissa” <melissa@melissajclark.ca>
2014-10-25 17:10:01 -0400

    adding more icon fields, and organizing alphabetically

--- themes/starter-theme/inc/social-media-fields.php
+++ themes/starter-theme/inc/social-media-fields.php
@@ -4,13 +4,7 @@
 
 		<!-- Important: Update links to assets before using! -->
 	
-			<!-- Twitter -->
 			
-			<?php if ( get_field( 'twitter', 'options' ) ) : ?>
-			<a href="<?php the_field( 'twitter', 'options' ); ?>" target="_blank" class="social-link" alt="twitter-link" >
-			    <i class="fa fa-twitter fa-2x" alt="twitter" class="social"></i>
-			    </a>
-			<?php endif; ?>
 
 			<!-- Instagram -->
 
@@ -20,19 +14,20 @@
 			    </a>
 			<?php endif; ?>
 
-			<!-- Tumblr -->
-
-			<?php if ( get_field( 'tumblr', 'options' ) ) : ?>
-			<a href="<?php the_field( 'tumblr', 'options' ); ?>" target="_blank" class="social-link" alt="tumblr-profile">
-			    <i class="fa fa-tumblr fa-2x" alt="tumblr" class="social"></i>
-			    </a>
-			<?php endif; ?>
-
+			
 			<!-- Linkedin -->
 
 			<?php if ( get_field( 'linkedin', 'options' ) ) : ?>
 			<a href="<?php the_field( 'linkedin', 'options' ); ?>" target="_blank" class="social-link" alt="linkedin-profile">
 			    <i class="fa fa-linkedin fa-2x" alt="linkedin" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- github -->
+
+			<?php if ( get_field( 'github', 'options' ) ) : ?>
+			<a href="<?php the_field( 'github', 'options' ); ?>" target="_blank" class="social-link" alt="github-profile">
+			    <i class="fa fa-github fa-2x" alt="github" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
@@ -57,6 +52,46 @@
 			<?php if ( get_field( 'email', 'options' ) ) : ?>
 			 <a href="mailto:<?php the_field( 'email', 'options' ); ?>" target="_blank" class="social-link" alt="email-link">
 			    <i class="fa fa fa-envelope-o fa-2x" alt="email" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- Pinterest -->
+
+			<?php if ( get_field( 'pinterest', 'options' ) ) : ?>
+			<a href="<?php the_field( 'pinterest', 'options' ); ?>" target="_blank" class="social-link" alt="pinterest-profile">
+			    <i class="fa fa-pinterest fa-2x" alt="pinterest" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- Tumblr -->
+
+			<?php if ( get_field( 'tumblr', 'options' ) ) : ?>
+			<a href="<?php the_field( 'tumblr', 'options' ); ?>" target="_blank" class="social-link" alt="tumblr-profile">
+			    <i class="fa fa-tumblr fa-2x" alt="tumblr" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- Twitter -->
+			
+			<?php if ( get_field( 'twitter', 'options' ) ) : ?>
+			<a href="<?php the_field( 'twitter', 'options' ); ?>" target="_blank" class="social-link" alt="twitter-link" >
+			    <i class="fa fa-twitter fa-2x" alt="twitter" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- vine -->
+
+			<?php if ( get_field( 'vine', 'options' ) ) : ?>
+			<a href="<?php the_field( 'vine', 'options' ); ?>" target="_blank" class="social-link" alt="vine-profile">
+			    <i class="fa fa-vine fa-2x" alt="vine" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- Youtube -->
+
+			<?php if ( get_field( 'youtube', 'options' ) ) : ?>
+			<a href="<?php the_field( 'youtube', 'options' ); ?>" target="_blank" class="social-link" alt="youtube-profile">
+			    <i class="fa fa-youtube fa-2x" alt="youtube" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
