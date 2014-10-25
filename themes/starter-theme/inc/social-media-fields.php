da0b23367654d7b124f6c83a8af550d8d4bc51b8
“Melissa” <melissa@melissajclark.ca>
2014-10-25 17:18:48 -0400

    added all social media profiles, organized alphabetically

--- themes/starter-theme/inc/social-media-fields.php
+++ themes/starter-theme/inc/social-media-fields.php
@@ -3,25 +3,57 @@
 	<!-- ============= Displays Content From Theme's Option Page: Social Media Links ================-->
 
 		<!-- Important: Update links to assets before using! -->
+
+
 	
-			
+			<!-- behance -->
 
-			<!-- Instagram -->
-
-			<?php if ( get_field( 'instagram', 'options' ) ) : ?>
-			<a href="<?php the_field( 'instagram', 'options' ); ?>" target="_blank" class="social-link" alt="instagram-profile">
-			    <i class="fa fa-instagram fa-2x" alt="instagram" class="social"></i>
+			<?php if ( get_field( 'behance', 'options' ) ) : ?>
+			<a href="<?php the_field( 'behance', 'options' ); ?>" target="_blank" class="social-link" alt="behance-profile">
+			    <i class="fa fa-behance fa-2x" alt="behance" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
-			
-			<!-- Linkedin -->
+			<!-- bitbucket -->
 
-			<?php if ( get_field( 'linkedin', 'options' ) ) : ?>
-			<a href="<?php the_field( 'linkedin', 'options' ); ?>" target="_blank" class="social-link" alt="linkedin-profile">
-			    <i class="fa fa-linkedin fa-2x" alt="linkedin" class="social"></i>
+			<?php if ( get_field( 'bitbucket', 'options' ) ) : ?>
+			<a href="<?php the_field( 'bitbucket', 'options' ); ?>" target="_blank" class="social-link" alt="bitbucket-profile">
+			    <i class="fa fa-bitbucket fa-2x" alt="bitbucket" class="social"></i>
 			    </a>
 			<?php endif; ?>
+
+			<!-- codepen -->
+
+			<?php if ( get_field( 'codepen', 'options' ) ) : ?>
+			<a href="<?php the_field( 'codepen', 'options' ); ?>" target="_blank" class="social-link" alt="codepen-profile">
+			    <i class="fa fa-codepen fa-2x" alt="codepen" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- dribble -->
+
+			<?php if ( get_field( 'dribble', 'options' ) ) : ?>
+			<a href="<?php the_field( 'dribble', 'options' ); ?>" target="_blank" class="social-link" alt="dribble-profile">
+			    <i class="fa fa-dribble fa-2x" alt="dribble" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- Email -->
+
+			<?php if ( get_field( 'email', 'options' ) ) : ?>
+			 <a href="mailto:<?php the_field( 'email', 'options' ); ?>" target="_blank" class="social-link" alt="email-link">
+			    <i class="fa fa fa-envelope-o fa-2x" alt="email" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- flickr -->
+
+			<?php if ( get_field( 'flickr', 'options' ) ) : ?>
+			<a href="<?php the_field( 'flickr', 'options' ); ?>" target="_blank" class="social-link" alt="flickr-profile">
+			    <i class="fa fa-flickr fa-2x" alt="flickr" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
 
 			<!-- github -->
 
@@ -46,12 +78,20 @@
 			    <i class="fa fa-google-plus fa-2x" alt="googleplus" class="social"></i>
 			    </a>
 			<?php endif; ?>
+			
+			<!-- Instagram -->
 
-			<!-- Email -->
+			<?php if ( get_field( 'instagram', 'options' ) ) : ?>
+			<a href="<?php the_field( 'instagram', 'options' ); ?>" target="_blank" class="social-link" alt="instagram-profile">
+			    <i class="fa fa-instagram fa-2x" alt="instagram" class="social"></i>
+			    </a>
+			<?php endif; ?>
+	
+			<!-- Linkedin -->
 
-			<?php if ( get_field( 'email', 'options' ) ) : ?>
-			 <a href="mailto:<?php the_field( 'email', 'options' ); ?>" target="_blank" class="social-link" alt="email-link">
-			    <i class="fa fa fa-envelope-o fa-2x" alt="email" class="social"></i>
+			<?php if ( get_field( 'linkedin', 'options' ) ) : ?>
+			<a href="<?php the_field( 'linkedin', 'options' ); ?>" target="_blank" class="social-link" alt="linkedin-profile">
+			    <i class="fa fa-linkedin fa-2x" alt="linkedin" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
@@ -60,6 +100,14 @@
 			<?php if ( get_field( 'pinterest', 'options' ) ) : ?>
 			<a href="<?php the_field( 'pinterest', 'options' ); ?>" target="_blank" class="social-link" alt="pinterest-profile">
 			    <i class="fa fa-pinterest fa-2x" alt="pinterest" class="social"></i>
+			    </a>
+			<?php endif; ?>
+
+			<!-- slideshare -->
+
+			<?php if ( get_field( 'slideshare', 'options' ) ) : ?>
+			<a href="<?php the_field( 'slideshare', 'options' ); ?>" target="_blank" class="social-link" alt="slideshare-profile">
+			    <i class="fa fa-slideshare fa-2x" alt="slideshare" class="social"></i>
 			    </a>
 			<?php endif; ?>
 
