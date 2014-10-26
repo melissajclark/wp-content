774b66e6fe9b707a428e4b1c3b185ce6f9c042a9
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-26 15:12:10 -0400

    commented out custom image size for slider - maybe this is the cause of isse #1?

--- themes/starter-theme/functions.php
+++ themes/starter-theme/functions.php
@@ -76,7 +76,7 @@ if ( ! function_exists( 'starter_theme_setup' ) ):
 		// Add custom image sizes
         	// add_image_size( &#039;name&#039;, 500, 300, true );
 		// custom image size for slider
-		add_image_size( 'slider', 550, 550 ); // 550 pixels wide by 550 pixels tall, soft proportional crop mode
+		// add_image_size( 'slider', 550, 550 ); // 550 pixels wide by 550 pixels tall, soft proportional crop mode
 	}
 endif; // starter_theme_setup
 add_action( 'after_setup_theme', 'starter_theme_setup' );
