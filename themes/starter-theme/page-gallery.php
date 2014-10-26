6eed83725941eca5e7b5e884789e281467609034
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-26 15:35:59 -0400

    put content above sliders

--- themes/starter-theme/page-gallery.php
+++ themes/starter-theme/page-gallery.php
@@ -18,7 +18,10 @@ get_header(); ?>
                 <h1 class="entry-title"><?php the_title(); ?></h1>
             </header><!-- .entry-header -->
 
+
             <div class="entry-content">
+
+            <?php the_content(); ?>
 
             <?php        // Custom Field Content for Images
 
@@ -37,8 +40,6 @@ get_header(); ?>
                 </div>
             <?php endif; ?>
 
-
-                <?php the_content(); ?>
                 <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
             </div><!-- .entry-content -->
         </article><!-- #post-<?php the_ID(); ?> -->
