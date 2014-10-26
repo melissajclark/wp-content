36273d142d9baa79ee8167672a064a61d8ddccdb
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-26 15:38:01 -0400

    new template for Woo slider only

---
+++ themes/starter-theme/page-wooslider.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: WooSlider Template
+ *
+ * This is the template that displays all pages by default.
+ *
+ * @package Starter_Theme
+ */
+
+get_header(); ?>
+
+<section id="primary" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header><!-- .entry-header -->
+
+            <div class="entry-content">
+
+            <?php the_content(); ?>
+
+            <h3>Woo Flexslider</h3>
+
+            <?php 
+
+            $images = get_field('gallery');
+
+            if( $images ): ?>
+                <div id="slider" class="flexslider">
+                    <ul class="slides">
+                        <?php foreach( $images as $image ): ?>
+                            <li>
+                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                                <p><?php echo $image['caption']; ?></p>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            
+        <?php endif; ?>
+
+                <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
+            </div><!-- .entry-content -->
+        </article><!-- #post-<?php the_ID(); ?> -->
+
+    <?php endwhile; // end of the loop. ?>
+
+</section><!-- #primary -->
+
+<?php get_footer(); ?>
