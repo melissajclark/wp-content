bf60709fbbd1dfcef4eaebb5684b0aaf3616a13f
“Melissa” <melissa@melissajclark.ca>
2014-10-29 11:20:31 -0400

    updated form page to match html markup of rest of site

--- themes/starter-theme/page-form.php
+++ themes/starter-theme/page-form.php
@@ -10,18 +10,30 @@
 acf_form_head(); ?>
 <?php get_header(); ?>
 
-    <div id="primary">
-        <div id="content" role="main">
+    <section id="primary">
 
-            <?php /* The loop */ ?>
-            <?php while ( have_posts() ) : the_post(); ?>
+        <?php /* The loop */ ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </header><!-- .entry-header -->
+            
+            <div class="entry-content">
+                <?php the_content(); ?>
 
                 <?php acf_form(); ?>
 
-            <?php endwhile; ?>
+            <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
 
-        </div><!-- #content -->
-    </div><!-- #primary -->
+            </div><!-- .entry-content -->
+        </article><!-- #post-<?php the_ID(); ?> -->
+
+
+        <?php endwhile; // end of the loop. ?>
+
+    </section><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
