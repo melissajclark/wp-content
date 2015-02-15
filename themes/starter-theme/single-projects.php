3bab7997ec7c317205c323e20c91850c722d8e32
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-15 00:57:56 -0500

    gets content from content-project file

---
+++ themes/starter-theme/single-projects.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Single post template
+ *
+ * @package StarterTheme
+ */
+
+get_header(); ?>
+
+<section id="primary">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php get_template_part( 'content/content-project' ); ?>
+
+        <?php comments_template( '', true ); ?>
+
+        <!-- You could also put some between-post links here (next post, previous post) -->
+
+    <?php endwhile; // end of the loop. ?>
+
+</section><!-- #primary -->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
