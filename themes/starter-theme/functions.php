3cbd2536ca00203dae1891a340abee000c53534f
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-15 00:24:29 -0500

    registered custom post type

--- themes/starter-theme/functions.php
+++ themes/starter-theme/functions.php
@@ -196,3 +196,44 @@ function starter_theme_custom_query( $query ) {
     return $query;
 }
 add_filter( 'pre_get_posts', 'starter_theme_custom_query' );
+
+
+/*==================================================
+=            Custom Post Type: Projects            =
+==================================================*/
+
+add_action('init', 'cptui_register_my_cpt_projects');
+function cptui_register_my_cpt_projects() {
+register_post_type('projects', array(
+'label' => 'Projects',
+'description' => '',
+'public' => true,
+'show_ui' => true,
+'show_in_menu' => true,
+'capability_type' => 'post',
+'map_meta_cap' => true,
+'hierarchical' => false,
+'rewrite' => array('slug' => 'projects', 'with_front' => true),
+'query_var' => true,
+'has_archive' => true,
+'supports' => array('title','editor','excerpt','trackbacks','custom-fields','comments','revisions','thumbnail','author','page-attributes','post-formats'),
+'labels' => array (
+  'name' => 'Projects',
+  'singular_name' => 'Project',
+  'menu_name' => 'Projects',
+  'add_new' => 'Add Project',
+  'add_new_item' => 'Add New Project',
+  'edit' => 'Edit',
+  'edit_item' => 'Edit Project',
+  'new_item' => 'New Project',
+  'view' => 'View Project',
+  'view_item' => 'View Project',
+  'search_items' => 'Search Projects',
+  'not_found' => 'No Projects Found',
+  'not_found_in_trash' => 'No Projects Found in Trash',
+  'parent' => 'Parent Project',
+)
+) ); }
+
+/*-----  End of Custom Post Type: Projects  ------*/
+
