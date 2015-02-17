012b2ebb4195c3002524a36b39d2830a729d0208
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-17 16:40:11 -0500

    trying to display taxonomy - this code does it as a link, want seperated list

--- themes/starter-theme/content/content-project.php
+++ themes/starter-theme/content/content-project.php
@@ -13,7 +13,25 @@
         <li><strong>Language:</strong> <?php the_field('language'); ?></li>
         <li><strong>People:</strong> <?php the_field('team_members'); ?></li>
         <li><strong>Location:</strong> <?php the_field('city'); ?></li>
+        
+        <?php
+            $taxonomy = 'status';
 
+            // get the term IDs assigned to post.
+            $post_terms = wp_get_object_terms( $post->ID, $taxonomy, array( 'fields' => 'ids' ) );
+            // separator between links
+            $separator = ', ';
+
+            if ( !empty( $post_terms ) && !is_wp_error( $post_terms ) ) {
+
+                $term_ids = implode( ',' , $post_terms );
+                $terms = wp_list_categories( 'title_li=&style=none&echo=0&taxonomy=' . $taxonomy . '&include=' . $term_ids );
+                $terms = rtrim( trim( str_replace( '<br />',  $separator, $terms ) ), $separator );
+
+                // display post categories
+                echo  $terms;
+            }
+        ?>
     </ul>
           
         <!-- <?php the_content(); ?> -->
