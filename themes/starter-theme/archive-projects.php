6c29d3c7e03f7d22150a593e66f0d2bb49077a4a
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-18 10:26:26 -0500

    new code - finally displays list of terms in a category woohoo

--- themes/starter-theme/archive-projects.php
+++ themes/starter-theme/archive-projects.php
@@ -18,36 +18,17 @@ get_header(); ?>
             get_template_part( 'inc/archive-header' );
         endif; ?>
 
-        <?php 
-        // no default values. using these as examples
-        $taxonomies = array( 
-            'status',
-        );
-
-        $args = array(
-            'orderby'           => 'name', 
-            'order'             => 'ASC',
-            'hide_empty'        => true, 
-            'exclude'           => array(), 
-            'exclude_tree'      => array(), 
-            'include'           => array(),
-            'number'            => '', 
-            'fields'            => 'all', 
-            'slug'              => '',
-            'parent'            => '',
-            'hierarchical'      => true, 
-            'child_of'          => 0, 
-            'get'               => '', 
-            'name__like'        => '',
-            'description__like' => '',
-            'pad_counts'        => false, 
-            'offset'            => '', 
-            'search'            => '', 
-            'cache_domain'      => 'core'
-        ); 
-
-        $terms = get_terms($taxonomies, $args);
-        ?>
+        <?php // displays list of terms in the "status" taxonomy
+             $terms = get_terms( 'status' );
+             if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+                 echo '<ul>';
+                 foreach ( $terms as $term ) {
+                   echo '<li>' . $term->name . '</li>';
+                    
+                 }
+                 echo '</ul>';
+             }
+                ?>
             
         <div class="filterControls clearfix">
 
