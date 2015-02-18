5765747deadf989bb1efe1ae4fbea9bee16379a1
“Melissa” <melissa@melissajclark.ca>
2015-02-18 17:00:40 -0500

    added spacing - still not working

--- themes/starter-theme/assets/filterMe.js
+++ themes/starter-theme/assets/filterMe.js
@@ -66,14 +66,14 @@ filterApp.init = function() { // this function holds everything to start the app
 		$("article.filterResultsCurrent").show(); 
 
 		// finds items NOT matching user's selection and hides them
-		$("article.filterableItem").not('[data-status="' + '"        "' + filterApp.sortChoiceType + '"       + '"]").css("background", "red");
+		$("article.filterableItem").not('[data-status="' + '        ' + filterApp.sortChoiceType + '       "]').css("background", "red");
 
-		console.log($("article.filterableItem").not('[data-status="' + '"        "' + filterApp.sortChoiceType + '"       + '"]"));
+		console.log($("article.filterableItem").not('[data-status="' + '        ' + filterApp.sortChoiceType + '       "]'));
 
 		//finds items matching user's selection and shows them
-		$("article.filterableItem").filter('[data-status="' + filterApp.sortChoiceType + '"]').css("background", "green");
+		$("article.filterableItem").filter('[data-status="' + '        ' + filterApp.sortChoiceType + '       "]').css("background", "green");
 
-		console.log( "user clicked on:" + '[data-status="' + filterApp.sortChoiceType + '"]');
+		console.log( 'user clicked on:' + '[data-status="' + '        ' + filterApp.sortChoiceType + '       "]');
 
 
 		// hides legend if "all" is selected + shows all items when all is selected
