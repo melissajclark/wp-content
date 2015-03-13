d3ef2ff7c00ca1a9b4a0b58cbb5e0f3b8d9af7b6
Melissa Jean Clark <melissa@melissajclark.ca>
2015-03-13 09:56:06 -0400

    reset to regular content from content.php

--- themes/starter-theme/content/content-project.php
+++ themes/starter-theme/content/content-project.php
@@ -1,19 +1,4 @@
-<!-- Individual Article / Project -->
-<article id="post-<?php the_ID(); ?>" data-status="<?php // displays status terms attached to post
-                                $terms = get_the_terms( $post->ID, 'status');
-                                                        
-                                if ( $terms && ! is_wp_error( $terms ) ) : 
-
-                                    $status_links = array();
-
-                                    foreach ( $terms as $term ) {
-                                        $status_links[] = $term->slug;
-                                    }
-                                                        
-                                    $on_status = join(",", $status_links);                                
-                                echo $on_status; ?><?php endif; ?>" <?php post_class("filterableItem");  
-    // end article info?>>
-
+<article id="post-<?php the_ID(); ?>" <?php post_class('filterableItem'); ?>>
     <header class="entry-header">
         <h1 class="entry-title">
             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
@@ -22,30 +7,7 @@
     </header><!-- .entry-header -->
 
     <div class="entry-content">
-    <ul style="list-style-type:none; display:inline-block; margin: 0em 0em 0em 0em; padding: 0 0 0 0;">
-        <li><strong>Date:</strong> <?php the_field('date'); ?></li>
-        <li><strong>Language:</strong> <?php the_field('language'); ?></li>
-        <li><strong>People:</strong> <?php the_field('team_members'); ?></li>
-        <li><strong>Location:</strong> <?php the_field('city'); ?></li>
-        <li><strong>Status:</strong><?php // displays status terms attached to post
-        $terms = get_the_terms( $post->ID, 'status');
-                                
-        if ( $terms && ! is_wp_error( $terms ) ) : 
-
-            $status_links = array();
-
-            foreach ( $terms as $term ) {
-                $status_links[] = $term->name;
-            }
-                                
-            $on_status = join(", ", $status_links);
-        ?>
-            <?php echo $on_status; ?>
-
-        <?php endif; ?></li>
-
-    </ul><!-- / list-->   
-        <!-- <?php the_content(); ?> -->
+        <?php the_content(); ?>
         <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'starter-theme' ) . '</span>', 'after' => '</div>' ) ); ?>
     </div><!-- .entry-content -->
 
@@ -61,4 +23,4 @@
             ?>
         </div>
     </footer><!-- #entry-meta -->
-</article><!-- #post-<?php the_ID(); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
