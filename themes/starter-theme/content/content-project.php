5893fe863d5f4d2bd7c7a14242d1043eaaf9e929
Melissa Jean Clark <melissa@melissajclark.ca>
2015-03-13 15:11:30 -0400

    appending data attr to article div

--- themes/starter-theme/content/content-project.php
+++ themes/starter-theme/content/content-project.php
@@ -1,26 +1,46 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('filterableItem'); ?>>
-    <header class="entry-header">
-        <h1 class="entry-title">
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </h1>
-        <span class="entry-date"><?php echo get_the_date(); ?></span>
-    </header><!-- .entry-header -->
+ <?php while ( have_posts() ) : the_post(); ?>
+        
+       <article id="post-<?php the_ID(); ?>" <?php post_class('filterableItem'); ?> 
+       
+       data-tools="<?php // displays values for custom taxonomy 'tools' language attached to post
+        $terms = get_the_terms( $post->ID, 'language');
+                                
+        if ( $terms && ! is_wp_error( $terms ) ) : 
 
-    <div class="entry-content">
-        <?php the_content(); ?>
-        <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'starter-theme' ) . '</span>', 'after' => '</div>' ) ); ?>
-    </div><!-- .entry-content -->
+            $status_links = array();
 
-    <footer class="entry-meta">
+            foreach ( $terms as $term ) {
+                $status_links[] = $term->name;
+            }
+                                
+            $on_status = join(",", $status_links);                                
+        echo $on_status; ?><?php endif; ?>">
 
-        <?php the_tags( '<div class="post-tags">' . __( 'Tagged: ', 'starter-theme' ) , ', ', '</div>' ); ?>
+           <header class="entry-header">
+               <h1 class="entry-title">
+                   <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+               </h1>
+               <span class="entry-date"><?php echo get_the_date(); ?></span>
+           </header><!-- .entry-header -->
 
-        <div class="comments-link">
-            <?php comments_popup_link( 
-                 __( 'Leave a comment', 'starter-theme' ), 
-                 __( '1 comment', 'starter-theme' ), 
-                 __( '% comments', 'starter-theme' ) ); 
-            ?>
-        </div>
-    </footer><!-- #entry-meta -->
-</article><!-- #post-<?php the_ID(); ?> -->
+           <div class="entry-content">
+               <?php the_content(); ?>
+               <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'starter-theme' ) . '</span>', 'after' => '</div>' ) ); ?>
+           </div><!-- .entry-content -->
+
+           <footer class="entry-meta">
+
+               <?php the_tags( '<div class="post-tags">' . __( 'Tagged: ', 'starter-theme' ) , ', ', '</div>' ); ?>
+
+               <div class="comments-link">
+                   <?php comments_popup_link( 
+                        __( 'Leave a comment', 'starter-theme' ), 
+                        __( '1 comment', 'starter-theme' ), 
+                        __( '% comments', 'starter-theme' ) ); 
+                   ?>
+               </div>
+           </footer><!-- #entry-meta -->
+       </article><!-- #post-<?php the_ID(); ?> -->
+
+<?php endwhile; ?>
+
