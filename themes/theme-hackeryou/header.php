66d0cc3b9c5fc33975c37979f073f6a22653e211
Melissa Jean Clark <melissa@melissajclark.ca>
2015-03-19 15:25:19 -0400

    added hackeryou theme to repo

---
+++ themes/theme-hackeryou/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<?php // Load Meta ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php  wp_title('|', true, 'right'); ?></title>
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+  <?php // Load our CSS ?>
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+
+  <?php wp_head(); ?>
+</head>
+
+
+<body <?php body_class(); ?>>
+
+<header>
+  <div class="container">
+    <h1>
+      <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
+        <?php bloginfo( 'name' ); ?>
+      </a>
+    </h1>
+
+    <?php wp_nav_menu( array(
+      'container' => false,
+      'theme_locations' => 'primary'
+    )); ?>
+  </div> <!-- /.container -->
+</header><!--/.header-->
+
