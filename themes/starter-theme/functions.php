e4c9f616a31ec6375c6cf4a8414da80365f78d61
Melissa Jean Clark <melissa@melissajclark.ca>
2015-03-19 12:29:18 -0400

    removed notes in enqueue scripts section

--- themes/starter-theme/functions.php
+++ themes/starter-theme/functions.php
@@ -158,20 +158,6 @@ function starter_theme_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	// below block of code - used for every JS / JQ file included, put all inside this function
-
-	// Optional: include conditional at beginning of function if script is only needed on a certain page:
-
-	// if ( is_front_page() ) : --> only include line if it is conditional
-
-	// wp_enqueue_script(
-	//	'theme',
-	//	get_template_directory_uri() . '/assets/theme.js',
-	//	array('jquery')
-	// );
-
-	// endif; --> only include line if it is conditional
-
 	wp_enqueue_script(
 		'flexslider', // woo flexslider
 		get_template_directory_uri() . '/assets/vendor/jquery.flexslider-min.js',
