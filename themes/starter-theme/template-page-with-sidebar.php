e428534fe4afa9e8018e9095ace5a5b11fd356fd
“Melissa” <melissa@melissajclark.ca>
2015-03-21 16:56:03 -0400

    new template for page with custom sidebar

---
+++ themes/starter-theme/template-page-with-sidebar.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * Template Name: Page with Sidebar
+ *
+ * @package Starter_Theme
+ */
+
+get_header(); ?>
+
+<div class="wrapper"> 
+    <div class="container containerWithAside">
+    	<div id="primary" role="main"> 
+        <?php while ( have_posts() ) : the_post(); ?>
+
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </header><!-- .entry-header -->
+
+                <div class="entry-content">
+                    <?php the_content(); ?>
+                    <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
+                </div><!-- .entry-content -->
+            </article><!-- #post-<?php the_ID(); ?> -->
+
+            <?php endwhile; // end of the loop. ?>
+        </div><!-- / #primary -->
+        <?php dynamic_sidebar('sidebar-2'); ?>
+    </div><!-- / #container -->   
+</div><!-- / #wrapper --> 
+
+<?php get_footer(); ?>
