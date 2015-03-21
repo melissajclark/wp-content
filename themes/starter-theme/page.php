d7c946d3fa2c43eb9aef6ff6a8875f21503f4ddb
“Melissa” <melissa@melissajclark.ca>
2015-03-21 15:19:55 -0400

    set up container div

--- themes/starter-theme/page.php
+++ themes/starter-theme/page.php
@@ -10,22 +10,22 @@
 get_header(); ?>
 
 <div id="primary" role="main">
+    <div class="container">
+        <?php while ( have_posts() ) : the_post(); ?>
 
-    <?php while ( have_posts() ) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </header><!-- .entry-header -->
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <h1 class="entry-title"><?php the_title(); ?></h1>
-            </header><!-- .entry-header -->
+                <div class="entry-content">
+                    <?php the_content(); ?>
+                    <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
+                </div><!-- .entry-content -->
+            </article><!-- #post-<?php the_ID(); ?> -->
 
-            <div class="entry-content">
-                <?php the_content(); ?>
-                <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
-            </div><!-- .entry-content -->
-        </article><!-- #post-<?php the_ID(); ?> -->
-
-    <?php endwhile; // end of the loop. ?>
-
+            <?php endwhile; // end of the loop. ?>
+    </div><!-- / container -->
 </div><!-- / #primary -->
 
 <?php get_footer(); ?>
