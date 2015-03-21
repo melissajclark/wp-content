8f00736845609b45d10d92e40e2fd2227db758c6
“Melissa” <melissa@melissajclark.ca>
2015-03-21 15:21:20 -0400

    set up container div

--- themes/starter-theme/archive-projects.php
+++ themes/starter-theme/archive-projects.php
@@ -10,39 +10,43 @@ get_header(); ?>
 
 <div id="primary" role="main">
 
-   <?php if ( have_posts() ) : ?>
-        <!-- there IS content for this query -->
+    <div class="container"> 
 
-        <?php // check if we're on an archive page
-        if ( is_archive() ) :
-            // if so, print the archive title before the loop begins
-            get_template_part( 'inc/archive-header' );
-        endif; ?>
-        
-      <!-- Begin Grid Archive -->
+       <?php if ( have_posts() ) : ?>
+            <!-- there IS content for this query -->
 
-      <?php get_template_part( 'content/content-project'); ?>
+            <?php // check if we're on an archive page
+            if ( is_archive() ) :
+                // if so, print the archive title before the loop begins
+                get_template_part( 'inc/archive-header' );
+            endif; ?>
+            
+          <!-- Begin Grid Archive -->
 
-      <!-- End of Grid Archive -->
+          <?php get_template_part( 'content/content-project'); ?>
 
-        <nav id="nav-below">
-            <div class="nav-previous"><?php next_posts_link( __( "Older posts", "starter-theme" ) ); ?></div>
-            <div class="nav-next"><?php previous_posts_link( __( "Newer posts", "starter-theme" ) ); ?></div>
-        </nav><!-- #nav-above -->
+          <!-- End of Grid Archive -->
 
-    <?php else : ?>
-        <!-- there IS NOT content for this query -->
+            <nav id="nav-below">
+                <div class="nav-previous"><?php next_posts_link( __( "Older posts", "starter-theme" ) ); ?></div>
+                <div class="nav-next"><?php previous_posts_link( __( "Newer posts", "starter-theme" ) ); ?></div>
+            </nav><!-- #nav-above -->
 
-        <article id="post-0" class="hentry post no-results not-found">
-            <header class="entry-header">
-                <h1><?php _e( "Oops!", "starter-theme" ); ?></h1>
-            </header><!-- .entry-header -->
+        <?php else : ?>
+            <!-- there IS NOT content for this query -->
 
-            <p><?php _e( "We can&#039;t find content for this page!", "starter-theme" ); ?></p>
-        </article><!-- #post-0 -->
+            <article id="post-0" class="hentry post no-results not-found">
+                <header class="entry-header">
+                    <h1><?php _e( "Oops!", "starter-theme" ); ?></h1>
+                </header><!-- .entry-header -->
 
-    <?php endif; ?>
+                <p><?php _e( "We can&#039;t find content for this page!", "starter-theme" ); ?></p>
+            </article><!-- #post-0 -->
 
+        <?php endif; ?>
+
+    </div><!-- / container -->
+    
 </div><!-- / #primary -->
 
 <?php get_footer(); ?>
