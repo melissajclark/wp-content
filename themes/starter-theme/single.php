fec36cacd31f7cd4aec4a716e82dd46397f58770
“Melissa” <melissa@melissajclark.ca>
2015-03-21 15:29:40 -0400

    added wrapper

--- themes/starter-theme/single.php
+++ themes/starter-theme/single.php
@@ -8,17 +8,21 @@
 get_header(); ?>
 
 <div id="primary" role="main">
+    <div class="wrapper"> 
+        <div class="container"> 
 
-    <?php while ( have_posts() ) : the_post(); ?>
+	    <?php while ( have_posts() ) : the_post(); ?>
 
-        <?php get_template_part( 'content/content' ); ?>
+	        <?php get_template_part( 'content/content' ); ?>
 
-        <?php comments_template( '', true ); ?>
+	        <?php comments_template( '', true ); ?>
 
-        <!-- You could also put some between-post links here (next post, previous post) -->
+	        <!-- You could also put some between-post links here (next post, previous post) -->
 
-    <?php endwhile; // end of the loop. ?>
+	    <?php endwhile; // end of the loop. ?>
 
-</div><!-- #primary -->
+        </div><!-- / container -->
+    </div><!-- / wrapper -->   
+</div><!-- / #primary -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
