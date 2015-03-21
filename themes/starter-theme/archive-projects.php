b412b20b4cd43114bb9c1fb8cc0f00d7ec3e601a
“Melissa” <melissa@melissajclark.ca>
2015-03-21 15:22:25 -0400

    addded wrapper div

--- themes/starter-theme/archive-projects.php
+++ themes/starter-theme/archive-projects.php
@@ -9,44 +9,44 @@
 get_header(); ?>
 
 <div id="primary" role="main">
+    <div class="wrapper"> 
+        <div class="container"> 
 
-    <div class="container"> 
+           <?php if ( have_posts() ) : ?>
+                <!-- there IS content for this query -->
 
-       <?php if ( have_posts() ) : ?>
-            <!-- there IS content for this query -->
+                <?php // check if we're on an archive page
+                if ( is_archive() ) :
+                    // if so, print the archive title before the loop begins
+                    get_template_part( 'inc/archive-header' );
+                endif; ?>
+                
+              <!-- Begin Grid Archive -->
 
-            <?php // check if we're on an archive page
-            if ( is_archive() ) :
-                // if so, print the archive title before the loop begins
-                get_template_part( 'inc/archive-header' );
-            endif; ?>
-            
-          <!-- Begin Grid Archive -->
+              <?php get_template_part( 'content/content-project'); ?>
 
-          <?php get_template_part( 'content/content-project'); ?>
+              <!-- End of Grid Archive -->
 
-          <!-- End of Grid Archive -->
+                <nav id="nav-below">
+                    <div class="nav-previous"><?php next_posts_link( __( "Older posts", "starter-theme" ) ); ?></div>
+                    <div class="nav-next"><?php previous_posts_link( __( "Newer posts", "starter-theme" ) ); ?></div>
+                </nav><!-- #nav-above -->
 
-            <nav id="nav-below">
-                <div class="nav-previous"><?php next_posts_link( __( "Older posts", "starter-theme" ) ); ?></div>
-                <div class="nav-next"><?php previous_posts_link( __( "Newer posts", "starter-theme" ) ); ?></div>
-            </nav><!-- #nav-above -->
+            <?php else : ?>
+                <!-- there IS NOT content for this query -->
 
-        <?php else : ?>
-            <!-- there IS NOT content for this query -->
+                <article id="post-0" class="hentry post no-results not-found">
+                    <header class="entry-header">
+                        <h1><?php _e( "Oops!", "starter-theme" ); ?></h1>
+                    </header><!-- .entry-header -->
 
-            <article id="post-0" class="hentry post no-results not-found">
-                <header class="entry-header">
-                    <h1><?php _e( "Oops!", "starter-theme" ); ?></h1>
-                </header><!-- .entry-header -->
+                    <p><?php _e( "We can&#039;t find content for this page!", "starter-theme" ); ?></p>
+                </article><!-- #post-0 -->
 
-                <p><?php _e( "We can&#039;t find content for this page!", "starter-theme" ); ?></p>
-            </article><!-- #post-0 -->
+            <?php endif; ?>
 
-        <?php endif; ?>
-
-    </div><!-- / container -->
-    
+        </div><!-- / container -->
+    </div><!-- / wrapper -->   
 </div><!-- / #primary -->
 
 <?php get_footer(); ?>
