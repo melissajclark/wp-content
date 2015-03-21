c4b529ef6345d25659812ef6f88a1fd135667444
“Melissa” <melissa@melissajclark.ca>
2015-03-21 15:17:45 -0400

    added div.container around content

--- themes/starter-theme/page-gallery.php
+++ themes/starter-theme/page-gallery.php
@@ -12,44 +12,43 @@ get_header(); ?>
 
 <div id="primary" role="main">
 
-    <?php while ( have_posts() ) : the_post(); ?>
+    <div class="container">
+        <?php while ( have_posts() ) : the_post(); ?>
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <h1 class="entry-title"><?php the_title(); ?></h1>
-            </header><!-- .entry-header -->
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </header><!-- .entry-header -->
 
-            <div class="entry-content">
+                <div class="entry-content">
 
-            <?php the_content(); ?>
+                <?php the_content(); ?>
 
-            <?php // gallery content begins here
+                <?php // gallery content begins here
 
-            $images = get_field('gallery');
+                $images = get_field('gallery');
 
-            if( $images ): ?>
-            <section class="wooSliderContainer">
-                <section class="flexslider">
-                    <ul class="slides">
-                        <?php foreach( $images as $image ): ?>
-                            <li>
-                                <img class="flexSliderImage" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                                <p><?php echo $image['caption']; ?></p>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
+                if( $images ): ?>
+                <section class="wooSliderContainer">
+                    <section class="flexslider">
+                        <ul class="slides">
+                            <?php foreach( $images as $image ): ?>
+                                <li>
+                                    <img class="flexSliderImage" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                                    <p><?php echo $image['caption']; ?></p>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </section>
                 </section>
-            </section>
-            <?php endif; ?>
+                <?php endif; ?>
 
-                <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
-            </div><!-- .entry-content -->
-        </article><!-- #post-<?php the_ID(); ?> -->
+                    <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
+                </div><!-- .entry-content -->
+            </article><!-- #post-<?php the_ID(); ?> -->
 
-    <?php endwhile; // end of the loop. ?>
-
+        <?php endwhile; // end of the loop. ?>
+    </div><!-- / container -->
 </div><!-- / #primary -->
-
-    
 
 <?php get_footer(); ?>
