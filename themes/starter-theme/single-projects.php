ae88c377cb202289cb86c503882df2c1fbe178da
“Melissa” <melissa@melissajclark.ca>
2015-03-21 15:19:20 -0400

    set up container div for content

--- themes/starter-theme/single-projects.php
+++ themes/starter-theme/single-projects.php
@@ -9,15 +9,17 @@ get_header(); ?>
 
 <div id="primary" role="main">
 
-    <?php while ( have_posts() ) : the_post(); ?>
+	<div class="container">
+        <?php while ( have_posts() ) : the_post(); ?>
 
         <?php get_template_part( 'content/content-project' ); ?>
 
         <?php comments_template( '', true ); ?>
 
-        <!-- You could also put some between-post links here (next post, previous post) -->
+        <?php // pagingation could go here ?>
 
-    <?php endwhile; // end of the loop. ?>
+        <?php endwhile; // end of the loop. ?>
+    </div><!-- / container -->
 
 </div><!-- / #primary -->
 <?php get_sidebar(); ?>
