79e77bddd381aa79a6d9f696cfcebcdf724cbc9d
“Melissa” <melissa@melissajclark.ca>
2015-03-21 15:30:20 -0400

    reduced indentation

--- themes/starter-theme/archive.php
+++ themes/starter-theme/archive.php
@@ -11,38 +11,38 @@ get_header(); ?>
     <div class="wrapper"> 
         <div class="container"> 
 
-           <?php if ( have_posts() ) : ?>
-                <!-- there IS content for this query -->
+       <?php if ( have_posts() ) : ?>
+            <!-- there IS content for this query -->
 
-                <?php // check if we're on an archive page
-                if ( is_archive() ) :
-                    // if so, print the archive title before the loop begins
-                    get_template_part( 'inc/archive-header' );
-                endif; ?>
-                
-              <!-- Begin Grid Archive -->
+            <?php // check if we're on an archive page
+            if ( is_archive() ) :
+                // if so, print the archive title before the loop begins
+                get_template_part( 'inc/archive-header' );
+            endif; ?>
+            
+          <!-- Begin Grid Archive -->
 
-              <?php get_template_part( 'inc/grid-archive-loop'); ?>
+          <?php get_template_part( 'inc/grid-archive-loop'); ?>
 
-              <!-- End of Grid Archive -->
+          <!-- End of Grid Archive -->
 
-                <nav id="nav-below">
-                    <div class="nav-previous"><?php next_posts_link( __( "Older posts", "starter-theme" ) ); ?></div>
-                    <div class="nav-next"><?php previous_posts_link( __( "Newer posts", "starter-theme" ) ); ?></div>
-                </nav><!-- #nav-above -->
+            <nav id="nav-below">
+                <div class="nav-previous"><?php next_posts_link( __( "Older posts", "starter-theme" ) ); ?></div>
+                <div class="nav-next"><?php previous_posts_link( __( "Newer posts", "starter-theme" ) ); ?></div>
+            </nav><!-- #nav-above -->
 
-            <?php else : ?>
-                <!-- there IS NOT content for this query -->
+        <?php else : ?>
+            <!-- there IS NOT content for this query -->
 
-                <article id="post-0" class="hentry post no-results not-found">
-                    <header class="entry-header">
-                        <h1><?php _e( "Oops!", "starter-theme" ); ?></h1>
-                    </header><!-- .entry-header -->
+            <article id="post-0" class="hentry post no-results not-found">
+                <header class="entry-header">
+                    <h1><?php _e( "Oops!", "starter-theme" ); ?></h1>
+                </header><!-- .entry-header -->
 
-                    <p><?php _e( "We can&#039;t find content for this page!", "starter-theme" ); ?></p>
-                </article><!-- #post-0 -->
+                <p><?php _e( "We can&#039;t find content for this page!", "starter-theme" ); ?></p>
+            </article><!-- #post-0 -->
 
-            <?php endif; ?>
+        <?php endif; ?>
 
         </div><!-- / container -->
     </div><!-- / wrapper -->   
