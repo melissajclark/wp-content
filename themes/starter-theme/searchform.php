f1c7e729456641746e1c903236a44943cd2a7331
Melissa Jean Clark <melissa@melissajclark.ca>
2015-03-21 20:45:53 -0400

    removed post type input

--- themes/starter-theme/searchform.php
+++ themes/starter-theme/searchform.php
@@ -9,5 +9,4 @@
     <label for="s" class="assistive-text"><?php _e( 'Search', 'starter-theme' ); ?></label>
     <input type="text" class="field" name="s" id="s" placeholder="" />
     <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'SEARCH', 'starter-theme' ); ?>" />
-	<input type="hidden" name="post_type[]" value="post" />	
 </form>
