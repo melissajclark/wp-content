7087666a664c666da324aa5c19725be993f59f37
“Melissa” <melissa@melissajclark.ca>
2015-03-21 22:43:31 -0400

    cleaned up

--- themes/starter-theme/archive-projects.php
+++ themes/starter-theme/archive-projects.php
@@ -12,23 +12,23 @@ get_header(); ?>
     <div class="container">
     	<div id="primary" role="main"> 
 
-       <?php if ( have_posts() ) : ?>
-            <!-- there IS content for this query -->
-
+       <?php if ( have_posts() ) : // there is content for this query?>
+            
             <?php // check if we're on an archive page
             if ( is_archive() ) :
                 // if so, print the archive title before the loop begins
                 get_template_part( 'inc/archive-header' );
             endif; ?>
-            
-            <?php // display the items ?>
-            <?php get_template_part( 'content', 'project'); ?>
 
-            <?php get_template_part('inc/pagination'); ?>
+                <?php while ( have_posts() ) : the_post(); // displays project items ?>
 
+                <?php get_template_part('content', 'project'); ?>
 
-        <?php else : ?>
-            <!-- there IS NOT content for this query -->
+                <?php endwhile; ?>
+
+            <?php get_template_part('inc/pagination'); // displays pagination ?>
+
+        <?php else : // nothing found ?>
 
             <article id="post-0" class="hentry post no-results not-found">
                 <header class="entry-header">
