6ee537262852a14c87080e0b7a8922bb7525cc39
“Melissa” <melissa@melissajclark.ca>
2015-03-26 09:30:12 -0400

    converted <i>'s to <span>'s

--- themes/starter-theme/inc/social-media-fields.php
+++ themes/starter-theme/inc/social-media-fields.php
@@ -4,104 +4,104 @@
 
 <?php if ( get_field( 'behance', 'options' ) ) : // <!-- behance --> ?>
 <a href="<?php the_field( 'behance', 'options' ); ?>" target="_blank" class="social-link" alt="behance-profile">
-    <i class="fa fa-behance fa-1x" alt="behance" class="social"></i>
+    <span class="fa fa-behance fa-1x" alt="behance" class="social"></span>
     </a>
 <?php endif; ?>
 
 
 <?php if ( get_field( 'bitbucket', 'options' ) ) : // <!-- bitbucket --> ?>
 <a href="<?php the_field( 'bitbucket', 'options' ); ?>" target="_blank" class="social-link" alt="bitbucket-profile">
-    <i class="fa fa-bitbucket fa-1x" alt="bitbucket" class="social"></i>
+    <span class="fa fa-bitbucket fa-1x" alt="bitbucket" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'codepen', 'options' ) ) : // <!-- codepen --> ?>
 <a href="<?php the_field( 'codepen', 'options' ); ?>" target="_blank" class="social-link" alt="codepen-profile">
-    <i class="fa fa-codepen fa-1x" alt="codepen" class="social"></i>
+    <span class="fa fa-codepen fa-1x" alt="codepen" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'dribble', 'options' ) ) : // <!-- dribble --> ?>
 <a href="<?php the_field( 'dribble', 'options' ); ?>" target="_blank" class="social-link" alt="dribble-profile">
-    <i class="fa fa-dribble fa-1x" alt="dribble" class="social"></i>
+    <span class="fa fa-dribble fa-1x" alt="dribble" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'email', 'options' ) ) : // <!-- Email --> ?>
  <a href="mailto:<?php the_field( 'email', 'options' ); ?>" target="_blank" class="social-link" alt="email-link">
-    <i class="fa fa fa-envelope-o fa-1x" alt="email" class="social"></i>
+    <span class="fa fa fa-envelope-o fa-1x" alt="email" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'flickr', 'options' ) ) : // <!-- flickr --> ?>
 <a href="<?php the_field( 'flickr', 'options' ); ?>" target="_blank" class="social-link" alt="flickr-profile">
-    <i class="fa fa-flickr fa-1x" alt="flickr" class="social"></i>
+    <span class="fa fa-flickr fa-1x" alt="flickr" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'github', 'options' ) ) : // <!-- github --> ?>
 <a href="<?php the_field( 'github', 'options' ); ?>" target="_blank" class="social-link" alt="github-profile">
-    <i class="fa fa-github fa-1x" alt="github" class="social"></i>
+    <span class="fa fa-github fa-1x" alt="github" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'goodreads', 'options' ) ) : // <!-- Good Reads --> ?>
 <a href="<?php the_field( 'goodreads', 'options' ); ?>" target="_blank" class="social-link" alt="good-reads-profile">
-    <i class="fa fa-book fa-1x" alt="goodreads" class="social"></i>
+    <span class="fa fa-book fa-1x" alt="goodreads" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'google_plus', 'options' ) ) : // <!-- Google Plus --> ?>
 <a href="<?php the_field( 'google_plus', 'options' ); ?>" target="_blank" class="social-link" alt="google-plus-profile">
-    <i class="fa fa-google-plus fa-1x" alt="googleplus" class="social"></i>
+    <span class="fa fa-google-plus fa-1x" alt="googleplus" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'instagram', 'options' ) ) : // <!-- Instagram --> ?>
 <a href="<?php the_field( 'instagram', 'options' ); ?>" target="_blank" class="social-link" alt="instagram-profile">
-    <i class="fa fa-instagram fa-1x" alt="instagram" class="social"></i>
+    <span class="fa fa-instagram fa-1x" alt="instagram" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'linkedin', 'options' ) ) : // <!-- Linkedin --> ?>
 <a href="<?php the_field( 'linkedin', 'options' ); ?>" target="_blank" class="social-link" alt="linkedin-profile">
-    <i class="fa fa-linkedin fa-1x" alt="linkedin" class="social"></i>
+    <span class="fa fa-linkedin fa-1x" alt="linkedin" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'pinterest', 'options' ) ) : // <!-- Pinterest --> ?>
 <a href="<?php the_field( 'pinterest', 'options' ); ?>" target="_blank" class="social-link" alt="pinterest-profile">
-    <i class="fa fa-pinterest fa-1x" alt="pinterest" class="social"></i>
+    <span class="fa fa-pinterest fa-1x" alt="pinterest" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'slideshare', 'options' ) ) : // <!-- slideshare --> ?>
 <a href="<?php the_field( 'slideshare', 'options' ); ?>" target="_blank" class="social-link" alt="slideshare-profile">
-    <i class="fa fa-slideshare fa-1x" alt="slideshare" class="social"></i>
+    <span class="fa fa-slideshare fa-1x" alt="slideshare" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'tumblr', 'options' ) ) : // <!-- Tumblr --> ?>
 <a href="<?php the_field( 'tumblr', 'options' ); ?>" target="_blank" class="social-link" alt="tumblr-profile">
-    <i class="fa fa-tumblr fa-1x" alt="tumblr" class="social"></i>
+    <span class="fa fa-tumblr fa-1x" alt="tumblr" class="social"></span>
     </a>
 <?php endif; ?>
 	
 <?php if ( get_field( 'twitter', 'options' ) ) : // <!-- Twitter -->   ?>
 <a href="<?php the_field( 'twitter', 'options' ); ?>" target="_blank" class="social-link" alt="twitter-link" >
-    <i class="fa fa-twitter fa-1x" alt="twitter" class="social"></i>
+    <span class="fa fa-twitter fa-1x" alt="twitter" class="social"></span>
     </a>
 <?php endif; ?>
 
 
 <?php if ( get_field( 'vine', 'options' ) ) : // <!-- vine --> ?>
 <a href="<?php the_field( 'vine', 'options' ); ?>" target="_blank" class="social-link" alt="vine-profile">
-    <i class="fa fa-vine fa-1x" alt="vine" class="social"></i>
+    <span class="fa fa-vine fa-1x" alt="vine" class="social"></span>
     </a>
 <?php endif; ?>
 
 <?php if ( get_field( 'youtube', 'options' ) ) : // <!-- Youtube --> ?>
 <a href="<?php the_field( 'youtube', 'options' ); ?>" target="_blank" class="social-link" alt="youtube-profile">
-    <i class="fa fa-youtube fa-1x" alt="youtube" class="social"></i>
+    <span class="fa fa-youtube fa-1x" alt="youtube" class="social"></span>
     </a>
 <?php endif; ?>
