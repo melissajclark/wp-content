6ea532d1318dbcc349497eee90577903d0681c7f
“Melissa” <melissa@melissajclark.ca>
2015-03-26 12:02:56 -0400

    removed unecessary plugins

--- plugins/quick-featured-images/admin/views/filter_search.php
+++
@@ -1,5 +0,0 @@
-<h4><?php echo $this->valid_filters[ 'filter_search' ]; ?></h4>
-<p>
-	<label for="th_search_term"><?php _e( 'Type in a search term', $this->plugin_slug ); ?></label>
-	<input type="text" id="th_search_term" name="search_term" value="<?php if ( $this->selected_search_term ) { echo $this->selected_search_term; } ?>" />
-</p>
--- plugins/quick-featured-images/admin/views/section_errormsg.php
+++
@@ -1,8 +0,0 @@
-<h3><?php _e( 'Error', $this->page_slug );?></h3>
-<div class="th_content_inside">
-	<h4><?php _e( 'Reason', $this->plugin_slug );?></h4>
-	<p class="failure"><?php print $msg; ?></p>
-	<h4><?php _e( 'Solution', $this->plugin_slug );?></h4>
-	<p class="success"><?php print $solution; ?></p>
-</div>
-<p><a class="button" href='<?php echo esc_url( admin_url( sprintf( 'admin.php?page=%s', $this->page_slug ) ) );?>'><?php _e( 'Start again', $this->plugin_slug );?></a></p>
