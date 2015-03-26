cb50f658390b6f078f46527020bf2f3be01bffe5
“Melissa” <melissa@melissajclark.ca>
2015-03-26 07:15:11 -0400

    added new filter to format page titles. allows for plugins to alter this. see: https://tommcfarlin.com/filter-wp-title/

--- themes/starter-theme/functions.php
+++ themes/starter-theme/functions.php
@@ -106,6 +106,47 @@ if(function_exists('acf_add_options_page')) {
  
 	acf_add_options_page('Theme Options');
 }
+
+
+/**
+ * See article: https://tommcfarlin.com/filter-wp-title/
+ * Provides a standard format for the page title depending on the view. This is
+ * filtered so that plugins can provide alternative title formats.
+ *
+ * @param       string    $title    Default title text for current view.
+ * @param       string    $sep      Optional separator.
+ * @return      string              The filtered title.
+ * @package     mayer
+ * @subpackage  includes
+ * @version     1.0.0
+ * @since       1.0.0
+ */
+function mayer_wp_title( $title, $sep ) {
+	global $paged, $page;
+
+	if ( is_feed() ) {
+		return $title;
+	} // end if
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		$title = "$title $sep $site_description";
+	} // end if
+
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 ) {
+		$title = sprintf( __( 'Page %s', 'mayer' ), max( $paged, $page ) ) . " $sep $title";
+	} // end if
+
+	return $title;
+
+} // end mayer_wp_title
+add_filter( 'wp_title', 'mayer_wp_title', 10, 2 );
+
 /*-----  End of Useful Functions to Override WP Defaults  ------*/
 
 
