e0ddc80b4b79f423df49ea21fa3420296ec37851
“Melissa” <melissa@melissajclark.ca>
2015-03-26 08:06:07 -0400

    set up customized excerpt

--- themes/starter-theme/functions.php
+++ themes/starter-theme/functions.php
@@ -204,6 +204,22 @@ function starter_theme_add_editor_fonts(){
 }
 add_action('after_theme_setup', 'starter_theme_add_editor_fonts');
 
+/**
+*
+* Customize the Excerpt
+*
+**/
+
+function custom_excerpt_length($length) {
+	return 75;
+}
+add_filter('excerpt_length', 'custom_excerpt_length', 999);
+
+function new_excerpt_more($more){
+	return ' <a class="read-more" href="' . get_permalink( get_the_ID() ) .'">' . __('Read More', 'starter-theme') . '</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
 /*-----  End of Useful Functions to Override WP Defaults  ------*/
 
 
