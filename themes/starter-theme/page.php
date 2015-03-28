665a45b6fbe7fc1e4733d6a0eab1e304eff81e18
“Melissa” <melissa@melissajclark.ca>
2015-03-28 16:51:55 -0400

    getting content from include now

--- themes/starter-theme/page.php
+++ themes/starter-theme/page.php
@@ -14,17 +14,7 @@ get_header(); ?>
     	<div id="primary" role="main"> 
         <?php while ( have_posts() ) : the_post(); ?>
 
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                </header><!-- .entry-header -->
-
-                <div class="entry-content">
-                    <?php get_template_part('inc/gallery'); ?>
-                    <?php the_content(); ?>
-                    <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
-                </div><!-- .entry-content -->
-            </article><!-- #post-<?php the_ID(); ?> -->
+            <?php get_template_part('content'); ?>
 
             <?php endwhile; // end of the loop. ?>
         </div><!-- / #primary -->
