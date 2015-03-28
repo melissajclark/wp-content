d32d749c1efc5105c0b0cfaf6ad54d55fc1bda32
“Melissa” <melissa@melissajclark.ca>
2015-03-28 17:08:12 -0400

    nothing

--- themes/starter-theme/page.php
+++ themes/starter-theme/page.php
@@ -13,7 +13,7 @@ get_header(); ?>
     <div class="container">
     	<div id="primary" role="main"> 
         <?php while ( have_posts() ) : the_post(); ?>
-
+			
             <?php get_template_part('content'); ?>
 
             <?php endwhile; // end of the loop. ?>
