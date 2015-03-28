cac8d625cdfdfbbf67e4faaf32d52fb72f0d117f
“Melissa” <melissa@melissajclark.ca>
2015-03-28 16:07:52 -0400

    registered flickity preview size

--- themes/starter-theme/functions.php
+++ themes/starter-theme/functions.php
@@ -74,7 +74,8 @@ if ( ! function_exists( 'starter_theme_setup' ) ):
 		// Add custom image sizes
         	// add_image_size( &#039;name&#039;, 500, 300, true );
 		// custom image size for slider
-		add_image_size( 'flickity_gallery', 9999, 600, false ); // 550 pixels wide by 550 pixels tall, soft proportional crop mode
+		add_image_size( 'flickity_gallery', 900, 600, true );
+		add_image_size( 'flickity_preview', 300, 200, true );
 	}
 endif; // starter_theme_setup
 add_action( 'after_setup_theme', 'starter_theme_setup' );
