665e02d4a63cb3a4091bd92604e16ed07f59e75c
“Melissa” <melissa@melissajclark.ca>
2015-03-28 16:52:28 -0400

    used to be page-gallery, now module for all pages

---
+++ themes/starter-theme/inc/gallery.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+*
+* Gallery Module
+*
+**/
+
+$images = get_field('gallery');
+
+if( $images ): ?>
+    <div class="gallery"> 
+        <?php foreach( $images as $image ): ?>
+            <div class="gallerySlide"> 
+                <img src="<?php echo $image['sizes']['flickity_gallery']; ?>" alt="<?php echo $image['alt']; ?>" />
+                <span class="overlay"><?php echo $image['caption']; ?></span>
+            </div>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
