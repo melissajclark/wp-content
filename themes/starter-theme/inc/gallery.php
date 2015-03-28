52edf0e45baaf29123d42328911ed24b82d3dd7f
“Melissa” <melissa@melissajclark.ca>
2015-03-28 17:01:54 -0400

    removed extra  space, updated class on main div to mainGallery

--- themes/starter-theme/inc/gallery.php
+++ themes/starter-theme/inc/gallery.php
@@ -8,7 +8,7 @@
 $images = get_field('gallery');
 
 if( $images ): ?>
-    <div class="gallery"> 
+    <div class="mainGallery"> 
         <?php foreach( $images as $image ): ?>
             <div class="gallerySlide"> 
                 <img src="<?php echo $image['sizes']['flickity_gallery']; ?>" alt="<?php echo $image['alt']; ?>" />
