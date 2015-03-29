5006375b74c13b78c25446ecfb23d31d008d4545
Melissa Jean Clark <melissa@melissajclark.ca>
2015-03-29 17:14:58 -0400

    added paginaton

--- themes/starter-theme/single-projects.php
+++ themes/starter-theme/single-projects.php
@@ -15,6 +15,8 @@ get_header(); ?>
 
 	        <?php get_template_part( 'content' ); ?>
 
+	        <?php get_template_part( 'inc/pagination' ); ?>
+
 	    <?php endwhile; // end of the loop. ?>
 
         </div><!-- / #primary -->
