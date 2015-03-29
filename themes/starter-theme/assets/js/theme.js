3037406b71da794e36d2b8d28c4e6a9848a98a3a
“Melissa” <melissa@melissajclark.ca>
2015-03-29 10:40:40 -0400

    removed excess options

--- themes/starter-theme/assets/js/theme.js
+++ themes/starter-theme/assets/js/theme.js
@@ -1,9 +1,7 @@
 // flickity setup
 $(document).ready(function(){
  
- $(".mainGallery").flickity({
-
-    	// options, defaults listed
+    $(".mainGallery").flickity({
 
     	accessibility: true,
     	// enable keyboard navigation, pressing left & right keys
@@ -14,63 +12,10 @@ $(document).ready(function(){
     	// or set time between advances in milliseconds
     	// i.e. `autoPlay: 1000` will advance every 1 second
 
-    	cellAlign: 'center',
-    	// alignment of cells, 'center', 'left', or 'right'
-    	// or a decimal 0-1, 0 is beginning (left) of container, 1 is end (right)
-
-    	imagesLoaded:false,
-
-    	cellSelector: '.gallerySlide',
+    	cellSelector: '.gallerySlide'
     	// specify selector for cell elements
 
-    	contain: false,
-    	// will contain cells to container
-    	// so no excess scroll at beginning or end
-    	// has no effect if wrapAround is enabled
-
-    	draggable: false,
-    	// enables dragging & flicking
-
-    	freeScroll: true,
-    	// enables content to be freely scrolled and flicked
-    	// without aligning cells
-
-    	friction: 0.3,
-    	// smaller number = easier to flick farther
-
-    	initialIndex: 1,
-    	// zero-based index of the initial selected cell
-
-    	percentPosition: false,
-    	// sets positioning in percent values, rather than pixels
-    	// Enable if items have percent widths
-    	// Disable if items have pixel widths, like images
-
-    	prevNextButtons: true,
-    	// creates and enables buttons to click to previous & next cells
-
-    	pageDots: false,
-    	// create and enable page dots
-
-    	resize: false,
-    	// listens to window resize events to adjust size & positions
-
-    	rightToLeft: false,
-    	// enables right-to-left layout
-
-    	setGallerySize: false,
-    	// sets the height of gallery
-    	// disable if gallery already has height set with CSS
-
-    	watchCSS: false,
-    	// watches the content of :after of the element
-    	// activates if #element:after { content: 'flickity' }
-    	// IE8 and Android 2.3 do not support watching :after
-    	// set watch: 'fallbackOn' to enable for these browsers
-
-    	wrapAround: true
-    	// at end of cells, wraps-around to first for infinite scrolling
-
      }); // end flickity
+
 }); // end doc ready
 
