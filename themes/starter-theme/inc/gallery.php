0177be63311bd42a3fe235b3888f726720df0e00
“Melissa” <melissa@melissajclark.ca>
2015-03-29 10:57:41 -0400

    renamed gallery container

--- themes/starter-theme/inc/gallery.php
+++ themes/starter-theme/inc/gallery.php
@@ -8,7 +8,7 @@
 $images = get_field('gallery');
 
 if( $images ): ?>
-    <div class="mainGallery"> 
+    <div class="galleryContainer"> 
         <?php foreach( $images as $image ): ?>
             <div class="gallerySlide"> 
                 <img src="<?php echo $image['sizes']['flickity_gallery']; ?>" alt="<?php echo $image['alt']; ?>" />
