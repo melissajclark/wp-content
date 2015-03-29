af6f7c7e32c98bd8fd0aed1b82e4e20945fb9430
“Melissa” <melissa@melissajclark.ca>
2015-03-29 11:34:40 -0400

    everything is working now, set it up

--- themes/starter-theme/assets/js/theme.js
+++ themes/starter-theme/assets/js/theme.js
@@ -6,14 +6,13 @@ $(document).ready(function(){
     	accessibility: true,
     	autoPlay: true,
         setGallerySize:false,
-        cellSelector: '.gallerySlide',
-        // initalIndex:2,
-        // cellAlign: 'center',
-        // pageDots:true,
-        // freeScroll:true,
-        // resize:true,
-        // draggable:false,
-        contain:true
+        initalIndex:2,
+        cellAlign: 'center',
+        pageDots:true,
+        freeScroll:true,
+        wrapAround:true,
+        resize:true,
+        draggable:false
 
      }); // end flickity
 
