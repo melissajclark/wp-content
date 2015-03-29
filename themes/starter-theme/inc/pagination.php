f68abe8ac3662cb492540b8c85d107bc69f8c67c
Melissa Jean Clark <melissa@melissajclark.ca>
2015-03-29 17:08:00 -0400

    renamed to older / newwer

--- themes/starter-theme/inc/pagination.php
+++ themes/starter-theme/inc/pagination.php
@@ -13,11 +13,11 @@
 	    <div class="nav-next"><?php next_post_link( '%link', __( 'Next', 'starter-theme' ) ); ?></div>
 	</nav>
 	
-<?php else : ?>
+<?php elseif (is_home() || is_archive() ) : ?>
 
 	<nav id="nav-below">
-		<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'themeTextDomain' ) ); ?></div>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'themeTextDomain' ) ); ?></div>
-	</nav><!-- #nav-above -->
+	    <div class="nav-previous"><?php previous_posts_link( __( 'Older', 'starter-theme' ) ); ?></div>
+	    <div class="nav-next"><?php next_posts_link( __( 'Newer', 'starter-theme' ) ); ?></div>
+	</nav>
 
 <?php endif; ?>
