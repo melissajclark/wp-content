91d50518f849a97bc9ec2adc106a721b86183179
“Melissa” <melissa@melissajclark.ca>
2015-03-29 11:30:22 -0400

    put class on image rather than div around image

--- themes/starter-theme/inc/gallery.php
+++ themes/starter-theme/inc/gallery.php
@@ -10,9 +10,7 @@ $images = get_field('gallery');
 if( $images ): ?>
     <div class="galleryContainer"> 
         <?php foreach( $images as $image ): ?>
-            <div class="gallerySlide"> 
-                <img src="<?php echo $image['sizes']['flickity_gallery']; ?>" alt="<?php echo $image['alt']; ?>" />
-            </div>
+                <img class="gallerySlide" src="<?php echo $image['sizes']['flickity_gallery']; ?>" alt="<?php echo $image['alt']; ?>" />
         <?php endforeach; ?>
     </div>
 <?php endif; ?>
