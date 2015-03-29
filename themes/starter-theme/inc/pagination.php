d3251f6e251ce2f11ad9b70355c9506931ec385f
Melissa Jean Clark <melissa@melissajclark.ca>
2015-03-29 16:56:44 -0400

    setting up if statement to control what pagination is used for different templates

--- themes/starter-theme/inc/pagination.php
+++ themes/starter-theme/inc/pagination.php
@@ -5,7 +5,19 @@
  * @package themeHandle
  */
 ?>
-<nav id="nav-below">
-	<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'themeTextDomain' ) ); ?></div>
-	<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'themeTextDomain' ) ); ?></div>
-</nav><!-- #nav-above -->
+
+<?php if ( is_singular() ) : ?>
+
+	<nav id="nav-below">
+	    <div class="nav-previous"><?php previous_post_link( '%link', __( 'Previous', 'starter-theme' ) ); ?></div>
+	    <div class="nav-next"><?php next_post_link( '%link', __( 'Next', 'starter-theme' ) ); ?></div>
+	</nav>
+	
+<?php else : ?>
+
+	<nav id="nav-below">
+		<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'themeTextDomain' ) ); ?></div>
+		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'themeTextDomain' ) ); ?></div>
+	</nav><!-- #nav-above -->
+
+<?php endif; ?>
