2f4b0eccc4e473507dc17185b1c2698177497e94
“Melissa” <melissa@melissajclark.ca>
2015-04-01 11:47:15 -0400

    removed flickity function

--- themes/starter-theme/assets/js/theme.js
+++ themes/starter-theme/assets/js/theme.js
@@ -1,21 +1,6 @@
-// flickity setup
+// theme js
 $(document).ready(function(){
  
-    $('.galleryContainer').flickity({
-
-    	accessibility: true,
-    	autoPlay: true,
-        setGallerySize:false,
-        // cellSelector:'.gallerySlide',
-        initalIndex:2,
-        cellAlign: 'center',
-        pageDots:true,
-        freeScroll:true,
-        wrapAround:true,
-        resize:true,
-        draggable:false
-
-     }); // end flickity
 
 }); // end doc ready
 
