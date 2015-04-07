77524a3a442409f24732531fb8693f15e259b868
Melissa Jean Clark <melissa@melissajclark.ca>
2015-04-06 22:17:47 -0400

    removed 2 more custom functions - not needed in all projects

--- themes/starter-theme/functions.php
+++ themes/starter-theme/functions.php
@@ -107,64 +107,6 @@ if(function_exists('acf_add_options_page')) {
 }
 
 
-/**
- * See article: https://tommcfarlin.com/filter-wp-title/
- * Provides a standard format for the page title depending on the view. This is
- * filtered so that plugins can provide alternative title formats.
- *
- * @param       string    $title    Default title text for current view.
- * @param       string    $sep      Optional separator.
- * @return      string              The filtered title.
- * @package     mayer
- * @subpackage  includes
- * @version     1.0.0
- * @since       1.0.0
- */
-function mayer_wp_title( $title, $sep ) {
-	global $paged, $page;
-
-	if ( is_feed() ) {
-		return $title;
-	} // end if
-
-	// Add the site name.
-	$title .= get_bloginfo( 'name' );
-
-	// Add the site description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) ) {
-		$title = "$title $sep $site_description";
-	} // end if
-
-	// Add a page number if necessary.
-	if ( $paged >= 2 || $page >= 2 ) {
-		$title = sprintf( __( 'Page %s', 'mayer' ), max( $paged, $page ) ) . " $sep $title";
-	} // end if
-
-	return $title;
-
-} // end mayer_wp_title
-add_filter( 'wp_title', 'mayer_wp_title', 10, 2 );
-
-
-/**
-*
-* Adds the post thumbnail to content in an RSS feed
-* 
-* Read more: http://code.tutsplus.com/tutorials/50-filters-of-wordpress-filters-21-30--cms-21298
-**/
-
-add_filter( 'the_content_feed', 'the_content_feed_rss' );
- 
-function the_content_feed_rss( $content ) {
-    $featured_image = '';
-    $featured_image = get_the_post_thumbnail( get_the_ID(), 'thumbnail', array( 'style' => 'float:left;margin-right:.75em;' ) );
-    $content = get_the_excerpt() . ' <a href="'. get_permalink() .'">' . __( 'Read More' ) . '</a>';
-    if( '' != $featured_image )
-        $content = '<div>' . $featured_image . $content . '<br style="clear:both;" /></div>';
-    return $content;
-}
-
 
 /**
 *
