74555cadc1aefdfeffc3917b2dfd3c68092b8106
“Melissa” <melissa@melissajclark.ca>
2015-04-07 08:13:07 -0400

    added text-domain next to strings

--- themes/starter-theme/inc/archive-header.php
+++ themes/starter-theme/inc/archive-header.php
@@ -2,10 +2,10 @@
     <h1 class="entry-title">
     <?php
         if ( is_category() ) :
-            single_cat_title( _e('Category: ') ); 
+            single_cat_title( _e('Category: ', 'starter-theme') ); 
 
         elseif ( is_tag() ) :
-            single_tag_title( _e('Tag: '));
+            single_tag_title( _e('Tag: ', 'starter-theme'));
 
         elseif ( is_author() ) :
             printf( __( 'Author: %s', 'starter-theme' ), '<span>' . get_the_author() . '</span>' );
