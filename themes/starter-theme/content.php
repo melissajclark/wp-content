76af4df605836f1dbb3478a1276d413a13957c95
Melissa Jean Clark <melissa@melissajclark.ca>
2015-04-06 21:34:46 -0400

    added span. name schema to title

--- themes/starter-theme/content.php
+++ themes/starter-theme/content.php
@@ -23,7 +23,7 @@
             <h1 class="entry-title"><?php the_title(); ?></h1>
         <?php else : ?>     
             <h2 class="entry-title">
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                <a href="<?php the_permalink(); ?>"><span itemprop="name"><?php the_title(); ?></span></a>
             </h2>
         <?php endif; ?>
 
