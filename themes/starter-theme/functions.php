2ef1f689dab62decd93fed520426577f72f4e2fa
“Melissa” <melissa@melissajclark.ca>
2015-04-07 12:20:52 -0400

    added support for title-tag

--- themes/starter-theme/functions.php
+++ themes/starter-theme/functions.php
@@ -49,6 +49,8 @@ if ( ! function_exists( 'starter_theme_setup' ) ):
 		// Enable support for Post Thumbnails on posts and pages
 		add_theme_support( 'post-thumbnails' );
 
+		add_theme_support( 'title-tag' );
+
 		// Enable support for Post Formats.
 		add_theme_support( 'post-formats', array( 
 			'aside', 
