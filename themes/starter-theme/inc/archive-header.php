68b8af3465c6eed168c1dbdc3e7de1d9b0f6afc1
Melissa Jean Clark <melissa@melissajclark.ca>
2015-04-06 22:54:02 -0400

    added info for category / tag archives

--- themes/starter-theme/inc/archive-header.php
+++ themes/starter-theme/inc/archive-header.php
@@ -2,10 +2,10 @@
     <h1 class="entry-title">
     <?php
         if ( is_category() ) :
-            single_cat_title();
+            single_cat_title( _e('Category: ') ); 
 
         elseif ( is_tag() ) :
-            single_tag_title();
+            single_tag_title( _e('Tag: '));
 
         elseif ( is_author() ) :
             printf( __( 'Author: %s', 'starter-theme' ), '<span>' . get_the_author() . '</span>' );
