d0b64ef64606e65116b8fc0439703eee7a7541f0
“Melissa” <melissa@melissajclark.ca>
2015-04-08 17:20:49 -0400

    changed to starter-theme from starter_theme

--- mu-plugins/must-use-plugins.php
+++ mu-plugins/must-use-plugins.php
@@ -1,2 +1,2 @@
 <?php // mu-plugins/load.php
-require WPMU_PLUGIN_DIR.'/starter_theme-functionality/starter_theme-functionality.php';
+require WPMU_PLUGIN_DIR.'/starter-theme-functionality/starter-theme-functionality.php';
