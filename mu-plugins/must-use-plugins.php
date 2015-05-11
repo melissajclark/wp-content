9a76d59f1da8cf6ecc545bc8e7eeb381639626b2
Melissa Jean Clark <melissa@melissajclark.ca>
2015-05-11 10:58:42 -0400

    added description so its visible in wp-admin

--- mu-plugins/must-use-plugins.php
+++ mu-plugins/must-use-plugins.php
@@ -1,2 +1,15 @@
-<?php // mu-plugins/load.php
+<?php 
+/**
+ * Plugin Name:       Starter_Theme Functionality Plugin
+ * Description:       This plugin stores all custom functionality for the Starter_Theme website.
+ * Version:           1.0.0
+ * Author:            Melissa Jean Clark
+ * Author URI:        http://melissajclark.ca
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       starter-theme-functionality
+ * Domain Path:       /languages
+ */
+
+// mu-plugins/load.php
 require WPMU_PLUGIN_DIR.'/starter-theme-functionality/starter-theme-functionality.php';
