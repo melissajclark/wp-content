21b31c732f6395f4a5b1e5519b60798ead2b60a8
Melissa Jean Clark <melissa@melissajclark.ca>
2015-05-11 14:55:22 -0400

    removed contentWrapper div (unecessary) + reformatted comments

--- themes/starter-theme/index.php
+++ themes/starter-theme/index.php
@@ -1,52 +1,52 @@
 <?php
-
-/** * Main Template File *  
+/** 
+*
+* Main Template File  
+*
 * This file is used to display a page when nothing more specific matches a query. 
 * Learn more: http://codex.wordpress.org/Template_Hierarchy 
-* * @package Starter_Theme */
+*
+* @package Starter_Theme 
+*/
 
 get_header(); ?>
 
-<div class="contentWrapper"> 
-    <div class="contentContainer contentWithAside">
-        <div id="primary" role="main">
+<div class="contentContainer contentWithAside">
+    <div id="primary" role="main">
 
-       <?php if ( have_posts() ) : ?>
-            <!-- there IS content for this query -->
+   <?php if ( have_posts() ) : // display the content _if_ there are posts ?>
 
-            <?php // check if we're on an archive page
-            if ( is_archive() ) :
-                // if so, print the archive title before the loop begins
-                get_template_part( 'inc/archive-header' );
-            endif; ?>
+        <?php // check if we're on an archive page
+        if ( is_archive() ) :
+            // if so, print the archive title before the loop begins
+            get_template_part( 'inc/archive-header' );
+        endif; ?>
 
-            <?php /* Start the Loop */ ?>
-            <?php while ( have_posts() ) : the_post(); ?>
+        <?php /* Start the Loop */ ?>
+        <?php while ( have_posts() ) : the_post(); ?>
 
-                <?php get_template_part('content'); ?>
+            <?php get_template_part('content'); ?>
 
-            <?php endwhile; ?>
+        <?php endwhile; ?>
 
-            <?php get_template_part('inc/pagination'); ?>
-            
-        <?php else : ?>
-            <!-- there IS NOT content for this query -->
-
-            <article id="post-0" class="hentry post no-results not-found">
-                <header class="entry-header">
-                    <h1><?php _e( "Oops!", "starter-theme" ); ?></h1>
-                </header><!-- .entry-header -->
-
-                <p><?php _e( "We can&#039;t find content for this page!", "starter-theme" ); ?></p>
-            </article><!-- #post-0 -->
-
-        <?php endif; ?>
-
-        </div><!-- / #primary -->
-
-        <?php get_sidebar(); ?>
+        <?php get_template_part('inc/pagination'); ?>
         
-    </div><!-- / #contentContainer -->   
-</div><!-- / #contentWrapper --> 
+    <?php else : // no content here! ?>
+
+        <article id="post-0" class="hentry post no-results not-found">
+            <header class="entry-header">
+                <h1><?php _e( "Oops!", "starter-theme" ); ?></h1>
+            </header><!-- .entry-header -->
+
+            <p><?php _e( "We can&#039;t find content for this page!", "starter-theme" ); ?></p>
+        </article><!-- #post-0 -->
+
+    <?php endif; ?>
+
+    </div><!-- / #primary -->
+
+    <?php get_sidebar(); ?>
+    
+</div><!-- / #contentContainer -->   
 
 <?php get_footer(); ?>
