449b231b0c0a8bf46e82d9f11ce2aea9368759a3
Melissa Jean Clark <melissa@melissajclark.ca>
2015-05-11 10:56:49 -0400

    added function to set featured image automatically if image uploaded to post

--- mu-plugins/starter-theme-functionality/starter-theme-functionality.php
+++ mu-plugins/starter-theme-functionality/starter-theme-functionality.php
@@ -92,3 +92,28 @@ function starter_theme_widgets_init() {
     ) );
 }
 add_action( 'widgets_init', 'starter_theme_widgets_init' );
+
+/**
+*
+* Set Attached Image as Featured Image Automatically
+*
+**/
+
+function autoset_featured() {
+          global $post;
+          $already_has_thumb = has_post_thumbnail($post->ID);
+              if (!$already_has_thumb)  {
+              $attached_image = get_children( "post_parent=$post->ID&post_type=attachment&post_mime_type=image&numberposts=1" );
+                          if ($attached_image) {
+                                foreach ($attached_image as $attachment_id => $attachment) {
+                                set_post_thumbnail($post->ID, $attachment_id);
+                                }
+                           }
+                        }
+      }
+add_action('the_post', 'autoset_featured');
+add_action('save_post', 'autoset_featured');
+add_action('draft_to_publish', 'autoset_featured');
+add_action('new_to_publish', 'autoset_featured');
+add_action('pending_to_publish', 'autoset_featured');
+add_action('future_to_publish', 'autoset_featured');
