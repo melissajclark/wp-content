cbeeae32c1e751545f051d70a9d4ba77f5afefa2
Melissa Jean Clark <melissa@melissajclark.ca>
2015-05-11 15:00:03 -0400

    updated comments, combined #copyright + .contentContainer divs into one, no need for 2 divs

--- themes/starter-theme/footer.php
+++ themes/starter-theme/footer.php
@@ -7,21 +7,22 @@
  * @package Starter_Theme
  */
 ?>
- 
+
+<?php // close divs opened in header (#main + #page) ?>
+
 	</div><!-- #main -->
- 
 </div><!-- #page -->
  
 <footer id="colophon" role="contentinfo" class="contentWrapper">
-
-	<div class="contentContainer">
 		
-		<div id="copyright">
+	<div id="copyright" class="contentContainer">
+
 		<?php get_template_part ('inc/social-media-fields') // grabs theme's social media content - [TO DO] update icon assets in final theme?>
 			&copy; <?php echo date("Y"); ?> <?php echo bloginfo( "name" ); ?>
-			<a href="http://melissajclark.ca" rel="bookmark">theme by Melissa</a>
-		</div> 
-	</div><!-- / contentContainer -->
+
+		<a href="http://melissajclark.ca" rel="bookmark">theme by Melissa</a>
+
+	</div><!-- / #copyright .contentContainer -->
 
 </footer><!-- #colophon -->
 
