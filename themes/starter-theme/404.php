0f1bdcbbc7f64940255d73c123b74b0375dfa681
Melissa Jean Clark <melissa@melissajclark.ca>
2015-07-31 22:00:48 -0400

    added minify task

--- themes/starter-theme/404.php
+++ themes/starter-theme/404.php
@@ -8,29 +8,20 @@
 get_header(); ?>
 
 <div class="contentContainer contentWithAside">
-
 	<div id="primary" role="main"> 
-
 	    <article id="post-0" class="post error404 not-found">
-
 		    <header class="entry-header">
 		        <h1 class="entry-title"><?php _e( 'Uh oh!', 'starter-theme' ); ?></h1>
 		    </header>
 
 		    <div class="entry-content">
-
 		        <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps a search would help?', 'starter-theme' ); ?></p>
-
 		        <?php get_search_form(); ?>
-
 		    </div><!-- .entry-content -->
-		    
 		</article><!-- #post-0 -->
-
     </div><!-- / #primary -->
 
     <?php get_sidebar(); ?>
-
 </div><!-- / .contentContainer -->   
    
 <?php get_footer(); ?>?>
