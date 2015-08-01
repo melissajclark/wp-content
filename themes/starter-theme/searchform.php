d1586dc8e4331bba77314a786bb370f298924102
Melissa Jean Clark <melissa@melissajclark.ca>
2015-07-31 21:58:32 -0400

    capital case on "SEARCH"

--- themes/starter-theme/searchform.php
+++ themes/starter-theme/searchform.php
@@ -8,6 +8,6 @@
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label for="s" class="assistive-text"><?php _e( 'Search', 'starter-theme' ); ?></label>
     <input type="text" class="field" name="s" id="s" placeholder="" />
-    <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'SEARCH', 'starter-theme' ); ?>" />
+    <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'starter-theme' ); ?>" />
 	<input type="hidden" name="post_type[]" value="post" />	
 </form>
