406774b2bc7b1851383c484781b852ab03530f07
Melissa Jean Clark <melissa@melissajclark.ca>
2015-07-31 21:56:37 -0400

    reduced indentation, tidied markup

--- themes/starter-theme/content.php
+++ themes/starter-theme/content.php
@@ -5,7 +5,7 @@
  *
  **/ ?>
    
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemtype="http://schema.org/Article">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemtype="http://schema.org/Article">
         
 <?php 
 /**
@@ -16,29 +16,27 @@
 *
 **/ ?>
     
-    <header class="entry-header">
+<header class="entry-header">
 
-        <?php 
-        if ( is_single() || is_page() ) : // no link around title if it is a single post ?>
-            <h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1>
-        <?php else : ?>     
-            <h2 class="entry-title" itemprop="name">
-                <a itemprop="url" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </h2>
-        <?php endif; ?>
+    <?php if ( is_single() || is_page() ) : // no link around title if it is a single post ?>
+        <h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1>
+    <?php else : ?>     
+        <h2 class="entry-title" itemprop="name">
+            <a itemprop="url" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h2>
+    <?php endif; ?>
 
-        <?php 
-            if ( is_single() || is_home() || is_archive() ) : ?>
+    <?php if ( is_single() || is_home() || is_archive() ) : // display entra meta on post / blog templates ?>
 
-            <span class="entry-meta">
-                <span class="entry-date" itemprop="datePublished" content="<?php echo get_the_date(); ?>"><?php echo get_the_date(); ?></span>
-                <span itemprop="author" itemscope itemtype="http://schema.org/Person"><?php the_author_posts_link(); ?></span>  
-            </span><!-- / entry-meta -->
+        <span class="entry-meta">
+            <span class="entry-date" itemprop="datePublished" content="<?php echo get_the_date(); ?>"><?php echo get_the_date(); ?></span>
+            <span itemprop="author" itemscope itemtype="http://schema.org/Person"><?php the_author_posts_link(); ?></span>  
+        </span><!-- / entry-meta -->
 
-        <?php else :  // do not display date ?>
-               
-        <?php endif; ?>
-    </header><!-- .entry-header -->
+    <?php else :  // do not display date ?>
+           
+    <?php endif; ?>
+</header><!-- .entry-header -->
 
 <?php 
 /**
@@ -47,27 +45,23 @@
  *
  **/ ?>
 
-    <div class="entry-content">
-    
-        <?php // displays the excerpt if it is an archive, otherwise shows the full content
+<div class="entry-content">
+    <?php if (is_archive() ) : // displays the excerpt if it is an archive, otherwise shows the full content ?>
+        <?php echo get_the_post_thumbnail('large') ?>
+        <p itemprop="description"><?php the_excerpt(); ?></p>
 
-            if (is_archive() ) : // checks if its an archive and shows content accordingly ?>
-                <?php echo get_the_post_thumbnail('large') ?>
-                <span itemprop="description"><?php the_excerpt(); ?></span>
+    <?php else : // for all other templates, show this content ?>
 
-            <?php else : // for all other templates, show this content ?>
+        <?php the_content(); ?>
 
-                <?php the_content(); ?>
+    <?php endif; ?>
 
-        <?php endif; ?>
+    <?php if ( is_page() ) : // include the page-links if it is a page ?>
 
-        <?php if ( is_page() ) : // include the page-links if it is a page ?>
+        <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'starter-theme' ) . '</span>', 'after' => '</div>' ) ); ?>
 
-            <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'starter-theme' ) . '</span>', 'after' => '</div>' ) ); ?>
-
-        <?php endif; ?>
-
-    </div><!-- .entry-content -->
+    <?php endif; ?>
+</div><!-- .entry-content -->
 
 <?php 
 /**
@@ -76,24 +70,23 @@
  *
  **/ ?>
 
-    <?php 
-      if ( is_single() || is_home() ) : ?>
+<?php if ( is_single() || is_home() ) : ?>
 
-        <footer class="entry-meta">
-            <p><?php _e('Category: ', 'starter-theme'); ?><?php the_category(', '); ?></p>
-            <p><?php the_tags( '<div class="post-tags">' . __( 'Tags: ', 'starter-theme' ) , ', ', '</div>' ); ?></p>
+    <footer class="entry-meta">
+        <p><?php _e('Category: ', 'starter-theme'); ?><?php the_category(', '); ?></p>
+        <p><?php the_tags( '<div class="post-tags">' . __( 'Tags: ', 'starter-theme' ) , ', ', '</div>' ); ?></p>
 
-            <div class="comments-link">
-                <?php comments_popup_link( 
-                     __( 'Leave a comment', 'starter-theme' ), 
-                     __( '1 comment', 'starter-theme' ), 
-                     __( '% comments', 'starter-theme' ) ); 
-                ?>
-            </div>
-        </footer><!-- #entry-meta -->
+        <div class="comments-link">
+            <?php comments_popup_link( 
+                 __( 'Leave a comment', 'starter-theme' ), 
+                 __( '1 comment', 'starter-theme' ), 
+                 __( '% comments', 'starter-theme' ) ); 
+            ?>
+        </div>
+    </footer><!-- #entry-meta -->
 
-        <?php else : // doesn't match? don't show anything! ?>
+    <?php else : // doesn't match? don't show anything! ?>
 
-    <?php endif; // end footer entry-meta conditional ?>
+<?php endif; // end footer entry-meta conditional ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
