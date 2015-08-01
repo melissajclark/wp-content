bd7914ebdb337e1dedbae377aa5c28eee07181f5
Melissa Jean Clark <melissa@melissajclark.ca>
2015-07-31 21:46:11 -0400

    set up output of minified and regular CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
+    minifycss    = require('gulp-minify-css'),
     autoprefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglifyjs'),
     newer = require('gulp-newer');
 
+// output minifined version of SCSS to theme
+// ----------------------------------------
+gulp.task('styles-min', function(){
+  return gulp.src(['themes/starter-theme/scss/*.scss',
+                   'themes/starter-theme/scss/**/*.scss'], 
+            {base: 'themes/starter-theme/scss/'} )
+      .pipe(plumber())
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('themes/starter-theme/'));
+});
+
+// output regular version of CSS to assets
+// --------------------------------------
 gulp.task('styles', function(){
   return gulp.src(['themes/starter-theme/scss/*.scss',
                    'themes/starter-theme/scss/**/*.scss'], 
@@ -12,21 +27,12 @@ gulp.task('styles', function(){
       .pipe(plumber())
         .pipe(sass({ style: 'expanded' }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('themes/starter-theme'));
+        .pipe(gulp.dest('themes/starter-theme/assets/css/'));
 });
 
 gulp.task('watch', function() {
-      // Watch .scss files
-      gulp.watch('themes/starter-theme/scss/*.scss', ['styles']);
-      gulp.watch('themes/starter-theme/scss/**/*.scss', ['styles']);
-});
-
-gulp.task('uglify', function() {
-  gulp.src('themes/starter-theme/assets/js/*.js')
-    .pipe(uglify('themes/starter-theme/assets/js/scripts.min.js', {
-      outSourceMap: false
-    }))
+      gulp.watch('themes/starter-theme/**/*.scss', ['styles']);
 });
 
 
-gulp.task('default', ['styles', 'uglify', 'watch', ]);
+gulp.task('default', ['styles', 'styles-min', 'watch']);
