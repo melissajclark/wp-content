b32b0690f3f26ca7de3e7a17ed2e5822bab8f608
Melissa Jean Clark <melissa@melissajclark.ca>
2015-10-22 15:37:11 -0400

    removed cotnent with sidebar class on parent div of page

--- themes/starter-theme/404.php
+++ themes/starter-theme/404.php
@@ -7,21 +7,19 @@
 
 get_header(); ?>
 
-<div class="container contentWithAside">
-	<div id="primary" role="main"> 
-	    <article id="post-0" class="post error404 not-found">
-		    <header class="entry-header">
-		        <h1 class="entry-title"><?php _e( 'Uh oh!', 'starter-theme' ); ?></h1>
-		    </header>
+<div id="primary" role="main"> 
+    <article id="post-0" class="post error404 not-found">
+	    <header class="entry-header">
+	        <h1 class="entry-title"><?php _e( 'Uh oh!', 'starter-theme' ); ?></h1>
+	    </header>
 
-		    <div class="entry-content">
-		        <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps a search would help?', 'starter-theme' ); ?></p>
-		        <?php get_search_form(); ?>
-		    </div><!-- .entry-content -->
-		</article><!-- #post-0 -->
-    </div><!-- / #primary -->
+	    <div class="entry-content">
+	        <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps a search would help?', 'starter-theme' ); ?></p>
+	        <?php get_search_form(); ?>
+	    </div><!-- .entry-content -->
+	</article><!-- #post-0 -->
+</div><!-- / #primary -->
 
-    <?php get_sidebar(); ?>
-</div><!-- / .container -->   
+<?php get_sidebar(); ?>
    
 <?php get_footer(); ?>
