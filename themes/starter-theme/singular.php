165eef3b1e173f72b9040906d8a9cdee7e2d62ac
Melissa Jean Clark <melissa@melissajclark.ca>
2015-11-22 15:26:40 -0500

    removed un-needed classes - better to customize for new projects

--- themes/starter-theme/singular.php
+++ themes/starter-theme/singular.php
@@ -1,25 +1,27 @@
 <?php
 /**
- * The Page template
+ * The Singular template
  *
- * This is the template that displays all pages by default.
+ * This is the template that displays all pages or posts by default.
  *
  * @package Starter_Theme
  */
 
 get_header(); ?>
 
-<div id="primary" role="main" class="container"> 
+<div id="primary" role="main"> 
 
 	<?php while ( have_posts() ) : the_post(); ?>
 		 <?php get_template_part('content'); ?>
 
-		    <?php if ( !is_singular('page') ) : // this is not a page ?>
-		        <?php comments_template( '', true ); ?>
-		    	<?php get_template_part( 'inc/pagination' ); ?>
-		    <?php endif; ?>
+	    <?php if ( !is_singular('page') ) : // this is not a page ?>
+	        <?php comments_template( '', true ); ?>
+	    	<?php get_template_part( 'inc/pagination' ); ?>
+	    <?php endif; ?>
 
 	 <?php endwhile; // end of the loop. ?>
+
+</div><!-- #primary -->
 
 	<?php if ( !is_singular('page') ) : // this is not a page ?>
 		<?php get_sidebar(); ?>
