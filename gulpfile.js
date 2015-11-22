ed851b0018c62f901d769e8aa3a164fc125f5158
Melissa Jean Clark <melissa@melissajclark.ca>
2015-11-22 16:35:50 -0500

    finally set up browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     minifycss    = require('gulp-minify-css'),
     autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
     newer = require('gulp-newer');
 
 gulp.task('styles', function(){
@@ -13,7 +14,14 @@ gulp.task('styles', function(){
         .pipe(sass({ style: 'expanded' }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         // .pipe(minifycss())
-        .pipe(gulp.dest('themes/starter-theme'));
+        .pipe(gulp.dest('themes/starter-theme'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "starter-theme"
+    });
 });
 
 
@@ -22,4 +30,4 @@ gulp.task('watch', function() {
       gulp.watch('themes/starter-theme/assets/scss/**/*.scss', ['styles']);
 });
 
-gulp.task('default', ['styles', 'watch' ]);
+gulp.task('default', ['styles', 'browser-sync', 'watch' ]);
