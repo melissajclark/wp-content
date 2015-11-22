9b5947fd5580e359274bb09607d1339fb57bd66a
Melissa Jean Clark <melissa@melissajclark.ca>
2015-11-22 15:23:44 -0500

    commented out minify task

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('styles', function(){
       .pipe(plumber())
         .pipe(sass({ style: 'expanded' }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(minifycss())
+        // .pipe(minifycss())
         .pipe(gulp.dest('themes/starter-theme'));
 });
 
